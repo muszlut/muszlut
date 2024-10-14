170ab1faa9f0a0575dbc3305f5d63bf9c63e7008
muszlut <mussegirma@yahoo.com>
2024-10-14 17:58:35 +0300

    edit

---
+++ ncbi_download_ethiopia.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=mustutorial         # Job name
+#SBATCH --partition=batch             # Partition (queue) name
+#SBATCH --ntasks=1                    # Run on a single CPU
+#SBATCH --mem=1gb                     # Job memory request
+#SBATCH --time=02:00:00               # Time limit hrs:min:sec
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out           # Standard output log
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err             # Standard error log
+
+#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=ma95362@uga.edu  # Where to send mail	
+
+#Set output directory variable
+OUTDIR="scratch/ma95362/ref"
+
+#Tell the program to make  the outdir folder
+if [ ! -d $OUTDIR ] 
+    then 
+        makdir -p $OUTDIR
+fi
+
+ #Load modules
+module load NCBI-Databasets-CLI/16.4.4
+
+#move to working directory
+cd $OUTDIR
+
+#download datasets and unzip
+datasets download genome accesssion GCF_000195955.2 --include cds, genome
+unzip ncbi_download.zip
