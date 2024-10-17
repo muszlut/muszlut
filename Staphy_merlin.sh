b0e7f3728f839879f9385fe827d7c60006e00bc4
muszlut <mussegirma@yahoo.com>
2024-10-17 19:34:21 +0300

    initail commit

---
+++ Staphy_merlin.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=staphy_merlin        # Job name
+#SBATCH --partition=batch             # Partition (queue) name
+#SBATCH --ntasks=1                    # Run on a single CPU
+#SBATCH --cpus-per-task=8               # Number of cores per task
+#SBATCH --mem=40gb                     # Job memory request
+#SBATCH --time=03:00:00               # Time limit hrs:min:sec
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out           # Standard output log
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err             # Standard error log
+
+#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=ma95362@uga.edu  # Where to send mail	
+
+#Set output directory variable
+OUTDIR="/scratch/ma95362/fastqs/paired_end_samples"
+
+#Tell the program to make  the outdir folder
+if [ ! -d $OUTDIR ] 
+    then 
+        mkdir -p $OUTDIR
+fi
+
+module load Bactopia/3.1.0
+cd $OUTDIR
+#ask merlin to run Staphy_ program 
+bactopia \
+    --wf 
