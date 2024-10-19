dc27502d8bd8e7e40986db08fe6cbde89ce46b2a
muszlut <mussegirma@yahoo.com>
2024-10-19 19:07:03 +0300

    initial commit

---
+++ snippy_PRJNA1056148.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=gbk_ncbi       # Job name
+#SBATCH --partition=batch             # Partition (queue) name
+#SBATCH --ntasks=1                    # Run on a single CPU
+#SBATCH --cpus-per-task=8               # Number of cores per task
+#SBATCH --mem=40gb                     # Job memory request
+#SBATCH --time=03:00:00               # Time limit hrs:min:sec
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out           # Standard output log
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err             # Standard error log
+
+#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=ma95362@uga.edu  # Where to send mail	
+#Set output directory variable
+OUTDIR="/scratch/ma95362/PRJNA1056148_bactopia"
+REF="/scratch/ma95362/gbk/ncbi_dataset/data/GCF_000195955.2"
+
+#Tell the program to make  the outdir folder
+if [ ! -d $OUTDIR ] 
+    then 
+        mkdir -p $OUTDIR
+fi
+
+module load Bactopia/3.1.0
+
+
+bactopia \
+    -profile singularity \
+    --wf tbprofiler \
+    --bactopia $OUTDIR/bactopia-exclude.tsv \
+    --bactopia $OUTDIR/ena-multiple-samples
