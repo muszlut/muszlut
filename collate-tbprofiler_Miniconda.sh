c7aa85ba8293e97fef2b0c5b4d2cf08b3eaa37ab
muszlut <mussegirma@yahoo.com>
2024-10-22 20:29:46 +0300

    initial commit

--- collate-tbprofiler_Miniconda.sh
+++ collate-tbprofiler_Miniconda.sh
@@ -12,5 +12,25 @@
 #SBATCH --mail-user=ma95362@uga.edu  # Where to send mail	
 #Set output directory variable
 OUTDIR="/scratch/ma95362/PRJNA1056148_bactopia"
-FOFN='/scratch/ma95362/
+FOFN='/scratch/ma95362/PRJNA1056148_bactopia/ena-multiple-samples/bactopia-runs/snippy-20241021-142944'
+
+
+#Tell the program to make  the outdir folder
+if [ ! -d $OUTDIR ] 
+    then 
+        mkdir -p $OUTDIR
+fi
+
+#Load modules
+module load Miniconda3/23.5.2-0
+
+#activate conda envirnoment (should be version 6.3.0)
+
+source activate tb-profiler-env
+
+#move to working directory:
+cd $OUTDIR
+
+#samples just needs to be a list of sample names. No path is required.
+tb-profiler collate --samples $FOFN/genomic.samples.txt --dir $OUTDIR/*/tools/tbprofiler --itol
 
