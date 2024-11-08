45965f342bb5b97f2f2051af4e2dc5c7d9455dfc
muszlut <mussegirma@yahoo.com>
2024-11-09 01:23:20 +0300

    initial commit

--- AHRI_Mtb_Data/collate-AHRI_tbprofiler_Miniconda.sh
+++ AHRI_Mtb_Data/collate-AHRI_tbprofiler_Miniconda.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
-#SBATCH --job-name=Bactopia_PRJNA1155881        # Job name
+#SBATCH --job-name=gbk_ncbi       # Job name
 #SBATCH --partition=batch             # Partition (queue) name
 #SBATCH --ntasks=1                    # Run on a single CPU
 #SBATCH --cpus-per-task=8               # Number of cores per task
 #SBATCH --mem=40gb                     # Job memory request
-#SBATCH --time=02-00:00:00               # Time limit hrs:min:sec
+#SBATCH --time=06:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/scratch/ma95362/scratch/log.%j.out           # Standard output log
 #SBATCH --error=/scratch/ma95362/scratch/log.%j.err             # Standard error log
 
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=ma95362@uga.edu  # Where to send mail	
-
 #Set output directory variable
-OUTDIR="/scratch/ma95362/PRJNA1155881_AHRI_bactopia"
+OUTDIR="/scratch/ma95362/PRJNA1155881_AHRI_bactopia/ena-multiple-samples"
+FOFN='/scratch/ma95362/PRJNA1155881_AHRI_bactopia/ena-multiple-samples/bactopia-runs/snippy-20241108-111044'
+
 
 #Tell the program to make  the outdir folder
 if [ ! -d $OUTDIR ] 
     then 
-        mkdir -p $OUTDIRcd
+        mkdir -p $OUTDIR
 fi
 
-module load Bactopia/3.1.0
+#Load modules
+module load Miniconda3/23.5.2-0
+
+#activate conda envirnoment (should be version 6.3.0)
+
+source activate tb-profiler-env
+
+#move to working directory:
 cd $OUTDIR
-bactopia summary --bactopia-path $OUTDIR/ena-multiple-samples/
-#bactopia search \
-#    --query PRJNA1155881
-#bactopia \
-#    --accessions $OUTDIR/bactopia-accessions.txt \
-#    --coverage 100 \
-#    --outdir $OUTDIR/ena-multiple-samples \
-#    --max_cpus 8
+
+#samples just needs to be a list of sample names. No path is required.
+tb-profiler collate --samples $FOFN/genomic.samples.txt --dir $OUTDIR/*/tools/tbprofiler --itol
+
