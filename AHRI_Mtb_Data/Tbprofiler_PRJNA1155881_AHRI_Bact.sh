fd2f2b405e80be83f0c10197e9d6ae612d4b1340
muszlut <mussegirma@yahoo.com>
2024-11-08 20:24:37 +0300

    job name changed

--- AHRI_Mtb_Data/Tbprofiler_PRJNA1155881_AHRI_Bact.sh
+++ AHRI_Mtb_Data/Tbprofiler_PRJNA1155881_AHRI_Bact.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=gbk_ncbi       # Job name
+#SBATCH --job-name=Tbprofiler_AHRI       # Job name
 #SBATCH --partition=batch             # Partition (queue) name
 #SBATCH --ntasks=1                    # Run on a single CPU
 #SBATCH --cpus-per-task=8               # Number of cores per task
