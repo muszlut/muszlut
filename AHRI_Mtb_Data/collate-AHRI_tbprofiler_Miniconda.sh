b7ec5428e29e5410105ae3b8d45368de3a691ae9
muszlut <mussegirma@yahoo.com>
2024-12-17 05:21:23 +0300

    initial commit

--- AHRI_Mtb_Data/collate-AHRI_tbprofiler_Miniconda.sh
+++ AHRI_Mtb_Data/collate-AHRI_tbprofiler_Miniconda.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=gbk_ncbi       # Job name
+#SBATCH --job-name=collate_Tbprofiler       # Job name
 #SBATCH --partition=batch             # Partition (queue) name
 #SBATCH --ntasks=1                    # Run on a single CPU
-#SBATCH --cpus-per-task=8               # Number of cores per task
-#SBATCH --mem=40gb                     # Job memory request
-#SBATCH --time=06:00:00               # Time limit hrs:min:sec
+#SBATCH --cpus-per-task=2               # Number of cores per task
+#SBATCH --mem=10gb                     # Job memory request
+#SBATCH --time=01:00:00               # Time limit hrs:min:sec
 #SBATCH --output=/scratch/ma95362/scratch/log.%j.out           # Standard output log
 #SBATCH --error=/scratch/ma95362/scratch/log.%j.err             # Standard error log
 
