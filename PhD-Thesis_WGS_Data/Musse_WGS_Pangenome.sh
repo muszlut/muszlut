18da71e73583dc9e1b03ac954c2068987253dc3f
muszlut <mussegirma@yahoo.com>
2024-12-27 03:54:42 +0300

    sec

--- PhD-Thesis_WGS_Data/Musse_WGS_Pangenome.sh
+++ PhD-Thesis_WGS_Data/Musse_WGS_Pangenome.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=Bactopia_Pangenome        # Job name
+#SBATCH --job-name=Musse_WGS_Pangenome        # Job name
 #SBATCH --partition=batch             # Partition (queue) name
 #SBATCH --ntasks=1                    # Run on a single CPU
 #SBATCH --cpus-per-task=8               # Number of cores per task
