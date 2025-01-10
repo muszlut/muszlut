545fda7b0f5c5fb2ec53c480fe38720344773111
muszlut <mussegirma@yahoo.com>
2025-01-10 06:05:20 +0300

    binary again

--- PhD-Thesis_WGS_Data/collate_spoligo.sh
+++ PhD-Thesis_WGS_Data/collate_spoligo.sh
@@ -33,7 +33,7 @@ for json_file in "$JSON_DIR"/*.spoligotype.json; do
     $json.spacers[] |
     {
         sample: $sample,
-        binary: ($json.binary | gsub(",";"")),
+        binary: ("\"\u2018" + $json.binary + "\u2019\""),
         octal: $json.octal,
         family: $json.family,
         SIT: $json.SIT,
