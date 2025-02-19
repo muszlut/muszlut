ce6305088ac1086794a7bffdb1826f82f4caddd9
muszlut <mussegirma@yahoo.com>
2025-02-19 20:56:10 +0300

    after chat gpt fastq format

--- PhD-Thesis_WGS_Data/Musse_WGS_Bac_Prepare_second.sh
+++ PhD-Thesis_WGS_Data/Musse_WGS_Bac_Prepare_second.sh
@@ -1,37 +1,47 @@
 #!/bin/bash
-#SBATCH --job-name=Musse_WGS_BacPrep_second                           # Job name
-#SBATCH --partition=batch                                      # Partition (queue) name
-#SBATCH --ntasks=1                                             # Run on a single CPU
-#SBATCH --cpus-per-task=8                                      # Number of cores per task
-#SBATCH --mem=40gb                                             # Job memory request
-#SBATCH --time=05-00:00:00                                     # Time limit hrs:min:sec
-#SBATCH --output=/scratch/ma95362/scratch/log.%j.out           # Standard output log
-#SBATCH --error=/scratch/ma95362/scratch/log.%j.err            # Standard error log
+#SBATCH --job-name=Musse_WGS_BacPrep_second                  # Job name
+#SBATCH --partition=batch                                    # Partition (queue) name
+#SBATCH --ntasks=1                                           # Run on a single CPU
+#SBATCH --cpus-per-task=8                                    # Number of cores per task
+#SBATCH --mem=40gb                                           # Job memory request
+#SBATCH --time=05-00:00:00                                   # Time limit (days-hours:minutes:seconds)
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out        # Standard output log
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err         # Standard error log
 
-#SBATCH --mail-type=END,FAIL                                   # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=ma95362@uga.edu                            # Where to send mail	
+#SBATCH --mail-type=END,FAIL                                 # Mail notifications (END, FAIL)
+#SBATCH --mail-user=ma95362@uga.edu                          # Email for notifications	
 
-#Set output directory variable
+# Define directories
+INPUT_DIR="/work/fdqlab/Ethiopia_wgs_mtb_2024/second_run"
 OUTDIR="/scratch/ma95362/musse_MGA/fastqs/Second_run"
 
-#Tell the program to make  the outdir folder
-if [ ! -d $OUTDIR ] 
-    then 
-        mkdir -p $OUTDIR
+# Create output directory if it does not exist
+if [ ! -d "$OUTDIR" ]; then
+    mkdir -p "$OUTDIR"
 fi
 
+# Load Bactopia module
 module load Bactopia/3.1.0
-cd $OUTDIR
+
+# Move to the output directory
+cd "$OUTDIR"
+
+# Prepare Bactopia samples list with FASTQ patterns
 bactopia prepare \
-    --path /work/fdqlab/Ethiopia_wgs_mtb_2024/second_run \
-    --species "Mycobacterium tuberculosis" \
-    --genome-size 4410000 \
-    > $OUTDIR/second_run_samples.txt
+    --path "$INPUT_DIR" \
+    --fastq-ext .fq.gz \
+    --pe1-pattern .1 \
+    --pe2-pattern .2 \
+    --outdir "$OUTDIR" \
+    > "$OUTDIR/second_run_samples.txt"
+
+# Run Bactopia with the prepared samples list
 bactopia \
-    --samples $OUTDIR/second_run_samples.txt\
+    --samples "$OUTDIR/second_run_samples.txt" \
     --coverage 100 \
-    --outdir $OUTDIR/local_multiple_samples \
+    --outdir "$OUTDIR/local_multiple_samples" \
     --max_cpus 4
+
 #bactopia summary \
 ##   --bactopia-path $OUTDIR/MGA_paired_end_samples
 
