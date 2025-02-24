2380c84d840dc6a615b6bc1a2728806d81115bc0
muszlut <mussegirma@yahoo.com>
2025-02-24 09:55:56 +0300

    Initial commit for snippy pangenome n tbprofiler

---
+++ PhD-Thesis_WGS_Data/Merged_Bactopia_Tbprofiler.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=Musse_Merged_BacPrep                           # Job name
+#SBATCH --partition=batch                                      # Partition (queue) name
+#SBATCH --ntasks=1                                             # Run on a single CPU
+#SBATCH --cpus-per-task=8                                      # Number of cores per task
+#SBATCH --mem=40gb                                             # Job memory request
+#SBATCH --time=05-00:00:00                                     # Time limit hrs:min:sec
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out           # Standard output log
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err            # Standard error log
+
+#SBATCH --mail-type=END,FAIL                                   # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=ma95362@uga.edu                            # Where to send mail	
+
+#Set output directory variable
+OUTDIR="/scratch/ma95362/musse_MGA/merged"
+
+# Tell the program to make the outdir folder
+if [ ! -d $OUTDIR ]; then 
+    mkdir -p $OUTDIR
+fi
+
+module load Bactopia/3.1.0
+
+bactopia \
+    -profile singularity \
+    --wf tbprofiler \
+    --exclude $OUTDIR/bactopia-exclude.tsv \
+    --bactopia $OUTDIR/MGA_paired_end_samples
