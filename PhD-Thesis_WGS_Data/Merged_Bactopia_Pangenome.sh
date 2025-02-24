df6769fa0e19895ecb473b66692f9f5c56ffffb3
muszlut <mussegirma@yahoo.com>
2025-02-24 10:31:48 +0300

    OUTDIR

--- PhD-Thesis_WGS_Data/Merged_Bactopia_Pangenome.sh
+++ PhD-Thesis_WGS_Data/Merged_Bactopia_Pangenome.sh
@@ -12,7 +12,7 @@
 #SBATCH --mail-user=ma95362@uga.edu                            # Where to send mail	
 
 #Set output directory variable
-OUTDIR="/scratch/ma95362/musse_MGA/merged"
+OUTDIR="/scratch/ma95362/musse_MGA/merged/MGA_paired_end_samples"
 EXCLUDE="/scratch/ma95362/musse_MGA/merged"
 
 
