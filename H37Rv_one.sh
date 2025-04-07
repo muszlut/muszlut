6e320f2b9b453ccbbf8de873c08da431e5cc5553
muszlut <mussegirma@yahoo.com>
2025-04-07 06:26:42 +0300

    comit

--- H37Rv_one.sh
+++ H37Rv_one.sh
@@ -26,5 +26,5 @@ module load NCBI-Datasets-CLI/16.4.4
 cd $OUTDIR
 
 #download datasets and unzip
-datasets download genome accession GCF_000195955.2 --include gff3
+datasets download genome accession GCF_000195955.2 --include gff3,fasta
 unzip ncbi_dataset.zip
