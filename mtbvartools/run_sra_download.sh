04500650db9b0798eea2bfb0b2127309ec97d73d
muszlut <mussegirma@yahoo.com>
2025-04-29 13:34:55 +0300

    second commit

--- mtbvartools/run_sra_download.sh
+++ mtbvartools/run_sra_download.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 #SBATCH --job-name=sra_download
-#SBATCH --partition=batch                             # Partition (queue) name
-#SBATCH --ntasks=1                                    # Run on a single CPU
-#SBATCH --cpus-per-task=8                             # Number of cores per task
-#SBATCH --mem=40GB                                    # Job memory request
-#SBATCH --time=07-00:00:00                            # Time limit hrs:min:sec
-#SBATCH --output=/scratch/ma95362/scratch/log.%j.out  # Standard output log
-#SBATCH --error=/scratch/ma95362/scratch/log.%j.err   # Standard error log
-
-#SBATCH --mail-type=END,FAIL                          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=ma95362@uga.edu                   # Where to send mail
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=40GB
+#SBATCH --time=07-00:00:00
+#SBATCH --output=/scratch/ma95362/scratch/logs/sra_download_%A_%a.out
+#SBATCH --error=/scratch/ma95362/scratch/logs/sra_download_%A_%a.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ma95362@uga.edu
+#SBATCH --array=1-5
 
 module purge
 module load python/3.10
 
-# (Optional) activate your environment
+# Activate your environment
 source activate mtbvartools
 
-# Create logs directory if not exists
-mkdir -p logs
+# Paths
+OUTPUT_DIR="/scratch/ma95362/Sequence"
+mkdir -p "$OUTPUT_DIR"
+cd "$OUTPUT_DIR"
 
-# Read the nth line of the SRA list (based on SLURM_ARRAY_TASK_ID)
+# Read current SRA ID
 SRA=$(sed -n "${SLURM_ARRAY_TASK_ID}p" sra_list.txt)
-
-# Set output name using the accession
 OUTPUT="sample_${SRA}"
 
-# Run your Python script
+# Run script
 python sra_download_script.py \
     -i $SRA \
     -o $OUTPUT \
