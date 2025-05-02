426f4756a18d4dae0f362410e3eb9d828ffaebbe
muszlut <mussegirma@yahoo.com>
2025-05-02 11:51:43 +0300

    third commit

--- M.bovis_Data/Sequence_download.sh
+++ M.bovis_Data/Sequence_download.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
-#SBATCH --job-name=Sequence read download      # Job name
-#SBATCH --partition=batch             # Partition (queue) name
-#SBATCH --ntasks=8                    # Run on a single CPU
-#SBATCH --mem=64gb                     # Job memory request
-#SBATCH --time=23:00:00               # Time limit hrs:min:sec
-#SBATCH --output=/scratch/ma95362/scratch/log.%j.out           # Standard output log
-#SBATCH --error=/scratch/ma95362/scratch/log.%j.err             # Standard error log
+#SBATCH --job-name=SequenceReadDownload   # Job name
+#SBATCH --partition=batch                 # Partition (queue) name
+#SBATCH --ntasks=8                         # Run on 8 CPU cores
+#SBATCH --mem=64gb                         # Job memory request
+#SBATCH --time=23:00:00                    # Time limit hrs:min:sec
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out  # Standard output log
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err   # Standard error log
 
-#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=ma95362@uga.edu  # Where to send mail	
+#SBATCH --mail-type=END,FAIL               # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=ma95362@uga.edu        # Where to send mail notifications
 
-#Set output directory variable
+# Set output directory variable
 OUTDIR="/scratch/ma95362/M.bovis_Ethiopia/Sequence_reads"
-#Set variable URL for genome website
-URL=" https://outlookuga-my.sharepoint.com/:f:/r/personal/ma95362_uga_edu/Documents/64%20SH%20Survey%20Samples?csf=1&web=1&e=XvK9PU "
 
-#Tell the program to make  the outdir folder
-if [ ! -d $OUTDIR ] 
-    then 
-        mkdir -p $OUTDIR
+# Set URL for genome website
+URL="https://outlookuga-my.sharepoint.com/:f:/r/personal/ma95362_uga_edu/Documents/64%20SH%20Survey%20Samples?csf=1&web=1&e=XvK9PU"
+
+# Create the output directory if it doesn't exist
+if [ ! -d "$OUTDIR" ]; then
+    mkdir -p "$OUTDIR"
 fi
 
-cd $OUTDIR
+# Change to output directory
+cd "$OUTDIR"
+
+# Attempt to download files from SharePoint
+echo "Downloading sequencing reads from SharePoint..."
+wget --no-check-certificate --recursive --level=1 --no-parent "$URL"
+
+echo "Download completed!"
