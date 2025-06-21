241da2a80f6e8ac8166a5a7195e6c21fb40cc2aa
muszlut <mussegirma@yahoo.com>
2025-06-21 15:55:13 +0300

    initial

--- M.bovis_Data/M.bovis_Bact_Prepare.sh
+++ M.bovis_Data/M.bovis_Bact_Prepare.sh
@@ -1,44 +1,42 @@
 #!/bin/bash
-#SBATCH --job-name=Musse_M.bovis_BacPrep                       # Job name
-#SBATCH --partition=batch                                      # Partition (queue) name
-#SBATCH --ntasks=1                                             # Run on a single CPU
-#SBATCH --cpus-per-task=8                                      # Number of cores per task
-#SBATCH --mem=40gb                                             # Job memory request
-#SBATCH --time=05-00:00:00                                     # Time limit hrs:min:sec
-#SBATCH --output=/scratch/ma95362/scratch/log.%j.out           # Standard output log
-#SBATCH --error=/scratch/ma95362/scratch/log.%j.err            # Standard error log
+#SBATCH --job-name=Musse_Mbovis_Bactopia                     # Job name
+#SBATCH --partition=batch                                    # Partition (queue) name
+#SBATCH --ntasks=1                                           # Run on a single CPU
+#SBATCH --cpus-per-task=8                                    # Number of cores per task
+#SBATCH --mem=40gb                                           # Job memory request
+#SBATCH --time=05-00:00:00                                   # Time limit d-hh:mm:ss
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out         # Standard output log
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err          # Standard error log
+#SBATCH --mail-type=END,FAIL                                 # Mail notifications
+#SBATCH --mail-user=ma95362@uga.edu                          # Your email address
 
-#SBATCH --mail-type=END,FAIL                                   # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=ma95362@uga.edu                            # Where to send mail	
+# Variables
+READS_DIR="/scratch/ma95362/ETH_bovis_Sequence/reads/64_SH_Sequence_data/raw/sub_raw"
+OUTDIR="/scratch/ma95362/ETH_bovis_Sequence/Abebe_samples"
 
-#Set output directory variable
-OUTDIR="/scratch/ma95362/ETH_bovis_Sequence"
+# Create output directory if it doesn't exist
+mkdir -p "$OUTDIR"
 
-#Tell the program to make  the outdir folder
-if [ ! -d $OUTDIR ] 
-    then 
-        mkdir -p $OUTDIR
-fi
-
+# Load Bactopia module
 module load Bactopia/3.1.0
+
+# Move to working directory
 cd $OUTDIR
+
+# Prepare samples for Bactopia
 bactopia prepare \
-    --path /scratch/ma95362/ETH_bovis_Sequence/reads/64_SH_Sequence_data/raw \
+    --path "$READS_DIR" \
     --species "Mycobacterium bovis" \
     --genome-size 4410000 \
-    > $OUTDIR/Mbovis_samples.txt
-bactopia \
-    --samples $OUTDIR/Mbovis_samples.txt \
-    --coverage 100 \
-    --outdir $OUTDIR/Mbovis_paired_end_samples \
-    --max_cpus 4
-bactopia summary \
-    --bactopia-path $OUTDIR/Mbovis_paired_end_samples
+    > "$OUTDIR/Mbovis_samples.txt"
 
-#bactopia search \
-#    --query PRJNA1155881
-#bactopia \
-#    --accessions $OUTDIR/bactopia-accessions.txt \
-#    --coverage 100 \
-#    --outdir $OUTDIR/ena-multiple-samples \
-#    --max_cpus 8
+# Run Bactopia pipeline
+bactopia \
+    --samples "$OUTDIR/Mbovis_samples.txt" \
+    --coverage 100 \
+    --outdir "$OUTDIR" \
+    --max_cpus 8
+
+# Generate summary
+bactopia summary \
+    --bactopia-path "$OUTDIR"
