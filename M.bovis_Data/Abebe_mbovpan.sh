70b48cbbe3c461c72cefaa3e8f71747976652970
muszlut <mussegirma@yahoo.com>
2025-06-24 01:09:55 +0300

    gemini

--- M.bovis_Data/Abebe_mbovpan.sh
+++ M.bovis_Data/Abebe_mbovpan.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=Abebe_mbovpan_pipeline                    # Job name
-#SBATCH --partition=batch                                    # Partition (queue) name
-#SBATCH --ntasks=1                                           # Run on a single CPU
-#SBATCH --cpus-per-task=32                                   # Number of cores per task
-#SBATCH --mem=40gb                                           # Job memory request
-#SBATCH --time=05-00:00:00                                   # Time limit d-hh:mm:ss
-#SBATCH --output=/scratch/ma95362/scratch/log.%j.out         # Standard output log
-#SBATCH --error=/scratch/ma95362/scratch/log.%j.err          # Standard error log
-#SBATCH --mail-type=END,FAIL                                 # Mail notifications
-#SBATCH --mail-user=ma95362@uga.edu                          # Your email address
+#SBATCH --job-name=Abebe_mbovpan_pipeline                    # Job name
+#SBATCH --partition=batch                                    # Partition (queue) name
+#SBATCH --ntasks=1                                           # Run on a single CPU
+#SBATCH --cpus-per-task=32                                   # Number of cores per task
+#SBATCH --mem=80gb                                           # Job memory request (Increased from 40GB)
+#SBATCH --time=05-00:00:00                                   # Time limit d-hh:mm:ss
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out         # Standard output log
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err          # Standard error log
+#SBATCH --mail-type=END,FAIL                                 # Mail notifications
+#SBATCH --mail-user=ma95362@uga.edu                          # Your email address
 
 OUTDIR="/scratch/ma95362/ETH_bovis_Sequence/Abebe_samples/mbovpan_result"
 
@@ -23,11 +23,12 @@ conda activate mbovpan-env
 cd /scratch/ma95362/mbovpan
 
 # Run the pipeline in full mode (spoligotyping + SNP + pangenome + virulence gene profiling)
+# Nextflow will now use the process-specific resource settings from nextflow.config
 nextflow run main.nf \
-  --input /scratch/ma95362/ETH_bovis_Sequence/mbovpanfastq_reads \
-  --output "$OUTDIR" \
-  --run all \
-  --threads 32 \
-  --qual 20 \
-  --depth 25 \
-  --mapq 40
+  --input /scratch/ma95362/ETH_bovis_Sequence/mbovpanfastq_reads \
+  --output "$OUTDIR" \
+  --run all \
+  --threads 32 \
+  --qual 20 \
+  --depth 25 \
+  --mapq 40
