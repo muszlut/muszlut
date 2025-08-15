9459ce43687611f200288fc5cc965e7be2db6a1f
muszlut <mussegirma@yahoo.com>
2025-08-15 18:35:08 +0300

    summary

--- all_Bactopia_Analysis/python_tbprofiler_custom_script.py
+++ all_Bactopia_Analysis/python_tbprofiler_custom_script.py
@@ -11,73 +11,62 @@ import pathogenprofiler as pp
 import tbprofiler as tbprofiler
 
 def main(args):
-    # Path to TBProfiler BED file
+    # Prepare locus tag -> drugs mapping
     bed_file = os.path.join(sys.base_prefix, "share", "tbprofiler", f"{args.db}.bed")
     locus_tag2drugs = tbprofiler.get_lt2drugs(bed_file)
 
-    # Collect sample files
+    # Get samples: either from file or by scanning the directory recursively
     if args.samples:
-        # If a sample list file is provided
-        samples_files = [x.rstrip() for x in open(args.samples).readlines()]
+        samples = [x.strip() for x in open(args.samples)]
+        files = [os.path.join(args.dir, s, "results", f"{s}.results.json") for s in samples]
     else:
-        # Recursively search for .results.json files in all subdirectories
-        samples_files = glob.glob(os.path.join(args.dir, "**", f"*{args.suffix}"), recursive=True)
+        files = glob.glob(os.path.join(args.dir, "**", f"*{args.suffix}"), recursive=True)
+        samples = [os.path.splitext(os.path.basename(f))[0].replace(args.suffix, "") for f in files]
 
-    if not samples_files:
-        print(f"No {args.suffix} files found in {args.dir}")
-        return
-
-    # Define drug sets
-    FLQ_set = {"moxifloxacin", "levofloxacin", "ciprofloxacin", "ofloxacin"}
-    GPA_set = {"bedaquiline", "linezolid"}
+    FLQ_set = {"moxifloxacin","levofloxacin","ciprofloxacin","ofloxacin"}
+    GPA_set = {"bedaquiline","linezolid"}
 
     # Open output CSV
-    with open(args.out, "w", newline="") as OUT:
-        writer = csv.DictWriter(OUT, fieldnames=["sample", "dr-class"])
-        writer.writeheader()
+    OUT = open(args.out, "w", newline='')
+    writer = csv.DictWriter(OUT, fieldnames=["sample","dr-class"])
+    writer.writeheader()
 
-        # Process each sample file
-        for sample_file in tqdm(samples_files, desc="Processing samples"):
-            # Extract sample name from filename
-            sample_name = os.path.splitext(os.path.basename(sample_file))[0].replace(args.suffix, "")
+    # Process each sample
+    for s, f in tqdm(zip(samples, files), total=len(samples), desc="Processing samples"):
+        data = json.load(open(pp.filecheck(f)))
+        resistant_drugs = set()
+        for var in data.get("dr_variants", []):
+            for d in var.get("drugs", []):
+                resistant_drugs.add(d["drug"])
 
-            # Load JSON data
-            data = json.load(open(pp.filecheck(sample_file)))
+        rif = "rifampicin" in resistant_drugs
+        inh = "isoniazid" in resistant_drugs
+        flq = len(FLQ_set.intersection(resistant_drugs)) > 0
+        gpa = len(GPA_set.intersection(resistant_drugs)) > 0
 
-            # Collect resistant drugs
-            resistant_drugs = {d["drug"] for var in data.get("dr_variants", []) for d in var.get("drugs", [])}
+        if len(resistant_drugs) == 0:
+            drtype = "Sensitive"
+        elif (rif and not inh) or (inh and not rif):
+            drtype = "Pre-MDR"
+        elif (rif and inh) and (not flq and not gpa):
+            drtype = "MDR"
+        elif (rif and inh) and (flq and not gpa):
+            drtype = "Pre-XDR"
+        elif (rif and inh) and (flq and gpa):
+            drtype = "XDR"
+        else:
+            drtype = "Other"
 
-            rif = "rifampicin" in resistant_drugs
-            inh = "isoniazid" in resistant_drugs
-            flq = bool(FLQ_set.intersection(resistant_drugs))
-            gpa = bool(GPA_set.intersection(resistant_drugs))
+        writer.writerow({"sample": s, "dr-class": drtype})
 
-            # Determine resistance class
-            if len(resistant_drugs) == 0:
-                drtype = "Sensitive"
-            elif (rif and not inh) or (inh and not rif):
-                drtype = "Pre-MDR"
-            elif (rif and inh) and (not flq and not gpa):
-                drtype = "MDR"
-            elif (rif and inh) and (flq and not gpa):
-                drtype = "Pre-XDR"
-            elif (rif and inh) and (flq and gpa):
-                drtype = "XDR"
-            else:
-                drtype = "Other"
-
-            # Write row to CSV
-            writer.writerow({"sample": sample_name, "dr-class": drtype})
+    OUT.close()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="TBProfiler merge script", 
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--out", type=str, help="Name of CSV output", required=True)
-    parser.add_argument("--samples", type=str, help="File with sample paths (optional)")
-    parser.add_argument("--dir", default="results/", type=str, help="Directory containing results")
-    parser.add_argument("--db", default="tbdb", type=str, help="Database name")
-    parser.add_argument("--suffix", default=".results.json", type=str, help="File suffix")
-    parser.set_defaults(func=main)
-
+    parser = argparse.ArgumentParser(description='TBProfiler summary script', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--out', type=str, required=True, help='Name of CSV output')
+    parser.add_argument('--samples', type=str, help='File listing samples (optional)')
+    parser.add_argument('--dir', default="results/", type=str, help='Directory containing results')
+    parser.add_argument('--db', default="tbdb", type=str, help='Database name')
+    parser.add_argument('--suffix', default=".results.json", type=str, help='File suffix')
     args = parser.parse_args()
-    args.func(args)
+    main(args)
