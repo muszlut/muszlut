2a8be48d35bb743dbb445c12930e0987f62d0e58
muszlut <mussegirma@yahoo.com>
2025-09-08 19:08:21 +0300

    combine initial

---
+++ M.bovis_Data/combine_Kraken_reprts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=kraken2_summary
+#SBATCH --partition=batch
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+#SBATCH --time=02:00:00
+#SBATCH --output=kraken2_summary.%j.out
+#SBATCH --error=kraken2_summary.%j.err
+
+# Load modules
+module load Kraken2/2.1.3-gompi-2023a
+module load Python/3.11.5
+
+# Path to your KrakenTools installation
+KRAKENTOOLS=/scratch/ma95362/KrakenTools   # <-- update if needed
+REPORTS_DIR=/scratch/ma95362/ETH_bovis_Sequence/reads/64_SH_Sequence_data/raw/fixed_fastqs/fixed_fastqs
+OUTFILE=/scratch/ma95362/ETH_bovis_Sequence/reads/64_SH_Sequence_data/raw/fixed_fastqs/fixed_fastqs/kraken2_combined.tsv
+
+# Combine reports into a single abundance table
+python $KRAKENTOOLS/combine_kreports.py \
+    -r ${REPORTS_DIR}/*.txt \
+    -o ${OUTFILE} \
+    -p
