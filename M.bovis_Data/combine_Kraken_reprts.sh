2f4dbf1a5e6e6bf3944172c36aebb71fa79497de
muszlut <mussegirma@yahoo.com>
2025-09-08 19:10:48 +0300

    tasks

--- M.bovis_Data/combine_Kraken_reprts.sh
+++ M.bovis_Data/combine_Kraken_reprts.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=kraken2_summary
 #SBATCH --partition=batch
+#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
 #SBATCH --time=02:00:00
-#SBATCH --output=kraken2_summary.%j.out
-#SBATCH --error=kraken2_summary.%j.err
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ma95362@uga.edu
 
 # Load modules
 module load Kraken2/2.1.3-gompi-2023a
