f857f625c7b768f7d8acf2ee948572594b38e2c3
muszlut <mussegirma@yahoo.com>
2025-09-22 15:35:10 +0300

    conda

--- M.bovis_Data/run_bovisanalyzer.sh
+++ M.bovis_Data/run_bovisanalyzer.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=bovisanalyzer
-#SBATCH --partition=batch
-#SBATCH --nodes=1
-#SBATCH --ntasks=16
-#SBATCH --time=72:00:00
-#SBATCH --mem=128G
-#SBATCH --output=bovisanalyzer_%j.out
-#SBATCH --error=bovisanalyzer_%j.err
+#SBATCH --job-name=bovis_analyzer                       # Job name
+#SBATCH --partition=batch                                # Partition (queue)
+#SBATCH --ntasks=1                                       # Single task
+#SBATCH --cpus-per-task=8                                # CPUs per task
+#SBATCH --mem=40gb                                       # Memory
+#SBATCH --time=05-00:00:00                               # Time limit (HH:MM:SS)
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out    # STDOUT log
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err     # STDERR log
+#SBATCH --mail-type=END,FAIL                             # Mail events
+#SBATCH --mail-user=ma95362@uga.edu                      # Your email
 
 # Load Anaconda / Miniconda
-module load anaconda/2020.11
+#module load anaconda/2020.11
 
 # Activate the bovisanalyzer environment
-source activate bovisanalyzer
+conda activate bovisanalyzer
 
 # Define paths
 SAMPLESHEET=/scratch/ma95362/ETH_M.bovis/m.bovis_Bactopia_Analysis/with_fixed_reads/M.bovis_paired_end_samples/samplesheet.csv
