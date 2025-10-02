d43f8c7637e0c5af457e977ea0ba3f624f8f9fd7
muszlut <mussegirma@yahoo.com>
2025-10-02 11:04:43 +0300

    again fix

--- M.bovis_Data/Kraken2_resume.sh
+++ M.bovis_Data/Kraken2_resume.sh
@@ -5,20 +5,19 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=200G
 #SBATCH --time=02-00:00:00
-#SBATCH --output=/scratch/ma95362/scratch/log.%j.out   # Standard output log
-#SBATCH --error=/scratch/ma95362/scratch/log.%j.err    # Standard error log
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=ma95362@uga.edu
 
-# Load Conda
-source $(conda info --base)/etc/profile.d/conda.sh
+# Load Conda properly
+source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate bovisanalyzer
 
-# Path
+# Path to Kraken2 DB
 KRAKEN2DB=/scratch/ma95362/kraken2_db
 
 # Continue/rebuild database.kraken
-kraken2-build --build --db $KRAKEN2DB --threads 16 --fast-build
+kraken2-build --build --db "$KRAKEN2DB" --threads 16 --fast-build
 
 echo "database.kraken finished!"
-```
