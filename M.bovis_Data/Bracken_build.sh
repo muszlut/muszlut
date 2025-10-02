026499c682a9a8e3f2d56e60aa400c03ae2b444a
muszlut <mussegirma@yahoo.com>
2025-10-02 11:14:25 +0300

    bracken build

--- M.bovis_Data/Bracken_build.sh
+++ M.bovis_Data/Bracken_build.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-#SBATCH --job-name=bracken_build          # Job name
-#SBATCH --partition=highmem_p             # High-memory partition
-#SBATCH --ntasks=1                        # Single task
-#SBATCH --cpus-per-task=8                 # Threads for Bracken
-#SBATCH --mem=120gb                       # Memory
-#SBATCH --time=05-00:00:00                # Time limit (HH:MM:SS)
-#SBATCH --output=/scratch/ma95362/bracken_build.%j.out
-#SBATCH --error=/scratch/ma95362/bracken_build.%j.err
+#SBATCH --job-name=bracken_build
+#SBATCH --partition=highmem_p
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=200G
+#SBATCH --time=02-00:00:00
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=ma95362@uga.edu
 
-# Load Conda and activate Bovisanalyzer environment
+# Load Conda
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate bovisanalyzer
 
-# Paths
+# Path
 KRAKEN2DB=/scratch/ma95362/kraken2_db
 
-# Build Bracken k-mer distribution
+# Bracken k-mer distribution build
 bracken-build \
     -d $KRAKEN2DB \
-    -t 8 \
+    -t 16 \
     -k 35 \
     -l 100
 
