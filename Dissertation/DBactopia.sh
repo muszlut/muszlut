904dd9e3bd18cffe7f6829ae20f3dbcc533aefa2
muszlut <mussegirma@yahoo.com>
2025-10-04 10:41:35 +0300

    initial

--- Dissertation/DBactopia.sh
+++ Dissertation/DBactopia.sh
@@ -12,7 +12,7 @@
 #SBATCH --mail-user=ma95362@uga.edu
 
 #Set output directory variable
-OUTDIR="/scratch/ma95362/Ethiopia_DBactopia_Analysis"
+OUTDIR="/scratch/ma95362/eth_national_analysis/all_fastq_reads"
 
 #Tell the program to make  the outdir folder
 if [ ! -d $OUTDIR ] 
@@ -28,17 +28,17 @@ module load Bactopia/3.2.0-conda
 cd $OUTDIR
 
 bactopia prepare \
-    --path /scratch/ma95362/eth_national_analysis/all_fastq_reads \
+    --path $OUTDIR \
     --species "Mycobacterium tuberculosis" \
     --genome-size 4410000 \
     > $OUTDIR/ETH_samples.txt
-#bactopia \
-#    --samples $OUTDIR/ETH_samples.txt \
-#    --coverage 100 \
-#    --outdir $OUTDIR/ETH_paired_end_samples \
-#    --max_cpus 4
-#bactopia summary \
-#    --bactopia-path $OUTDIR/ETH_paired_end_samples
-#bactopia plot \
-#    --bactopia-path $OUTDIR/ETH_paired_end_samples \
-#    --outdir $OUTDIR/ETH_paired_end_samples/plots
+bactopia \
+    --samples $OUTDIR/ETH_samples.txt \
+    --coverage 100 \
+    --outdir $OUTDIR/ETH_paired_end_samples \
+    --max_cpus 4
+bactopia summary \
+    --bactopia-path $OUTDIR/ETH_paired_end_samples
+bactopia plot \
+    --bactopia-path $OUTDIR/ETH_paired_end_samples \
+    --outdir $OUTDIR/ETH_paired_end_samples/plots
