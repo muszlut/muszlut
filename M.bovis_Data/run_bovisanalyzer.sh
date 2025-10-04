9b4eafb19cce9ef7c91eef461cc463d1dcc5cb79
muszlut <mussegirma@yahoo.com>
2025-10-04 22:43:34 +0300

    miniconda on

--- M.bovis_Data/run_bovisanalyzer.sh
+++ M.bovis_Data/run_bovisanalyzer.sh
@@ -7,27 +7,32 @@
 #SBATCH --time=05-00:00:00
 #SBATCH --output=/scratch/ma95362/scratch/log.%j.out
 #SBATCH --error=/scratch/ma95362/scratch/log.%j.err
+
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=ma95362@uga.edu
 
+
+# Load conda
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate bovisanalyzer
 
+# Define inputs
 SAMPLESHEET=/scratch/ma95362/ETH_M.bovis/m.bovis_Bactopia_Analysis/with_fixed_reads/M.bovis_paired_end_samples/samplesheet.csv
 REFERENCE=/scratch/ma95362/ETH_bovis_Sequence/bovis_REF/Fasta/AF2122_97.fasta
-KRAKEN2DB=/scratch/ma95362/kraken2_db/mini_db        # <-- mini DB
+KRAKEN2DB=/scratch/ma95362/kraken2_db/mini_db
 OUTDIR=/scratch/ma95362/ETH_bovis_Sequence/bovisanalyzer_output
 
-mkdir -p $OUTDIR
+# Create output and logs directory
+mkdir -p $OUTDIR/logs
 cd $OUTDIR
 
+# Run Bovisanalyzer
 nextflow run avantonder/bovisanalyzer \
     -profile conda \
     --input $SAMPLESHEET \
     --reference $REFERENCE \
     --kraken2db $KRAKEN2DB \
     --outdir $OUTDIR \
+    --threads 8 \
     -resume \
     -c /scratch/ma95362/bovisanalyzer_custom.config
-
-
