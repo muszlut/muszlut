c0aa724d1b4cb1bd4f06ec88c06c0282164477c1
muszlut <mussegirma@yahoo.com>
2025-10-04 15:22:28 +0300

    kraken skiped

--- M.bovis_Data/run_bovisanalyzer.sh
+++ M.bovis_Data/run_bovisanalyzer.sh
@@ -2,35 +2,34 @@
 #SBATCH --job-name=bovis_analyzer                  # Job name
 #SBATCH --partition=batch                          # Partition (queue)
 #SBATCH --ntasks=1                                 # Single task
-#SBATCH --cpus-per-task=8                          # CPUs per task (safe balance)
-#SBATCH --mem=180G                                 # Memory (more than before)
+#SBATCH --cpus-per-task=8                          # CPUs per task
+#SBATCH --mem=180G                                 # Memory
 #SBATCH --time=05-00:00:00                         # Time limit (HH:MM:SS)
 #SBATCH --output=/scratch/ma95362/scratch/log.%j.out   # STDOUT log
 #SBATCH --error=/scratch/ma95362/scratch/log.%j.err    # STDERR log
 #SBATCH --mail-type=END,FAIL                       # Mail events
 #SBATCH --mail-user=ma95362@uga.edu                # Your email
 
-# Initialize Conda in SLURM shell
+# Initialize Conda
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate bovisanalyzer
 
 # Define paths
 SAMPLESHEET=/scratch/ma95362/ETH_M.bovis/m.bovis_Bactopia_Analysis/with_fixed_reads/M.bovis_paired_end_samples/samplesheet.csv
 REFERENCE=/scratch/ma95362/ETH_bovis_Sequence/bovis_REF/Fasta/AF2122_97.fasta
-KRAKEN2DB=/scratch/ma95362/kraken2_db
 OUTDIR=/scratch/ma95362/ETH_bovis_Sequence/bovisanalyzer_output
+CONFIG=/scratch/ma95362/bovisanalyzer_custom.config
 
 # Make output directory if it doesn't exist
 mkdir -p $OUTDIR
 cd $OUTDIR
 
-# Run Bovisanalyzer with external config
+# Run Bovisanalyzer skipping Kraken2 & Bracken
 nextflow run avantonder/bovisanalyzer \
     -profile conda \
     --input $SAMPLESHEET \
     --reference $REFERENCE \
-    --kraken2db $KRAKEN2DB \
     --outdir $OUTDIR \
     -resume \
-    -c /scratch/ma95362/bovisanalyzer_custom.config
+    -c $CONFIG
 
