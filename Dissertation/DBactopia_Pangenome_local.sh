598ff8f302c48c14f7392334778109a5a11aa41b
muszlut <mussegirma@yahoo.com>
2025-10-08 14:08:52 +0300

    purge only

--- Dissertation/DBactopia_Pangenome_local.sh
+++ Dissertation/DBactopia_Pangenome_local.sh
@@ -5,8 +5,8 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=240G
 #SBATCH --time=7-00:00:00
-#SBATCH --output=/scratch/ma95362/scratch/log.%j.out   # STDOUT log
-#SBATCH --error=/scratch/ma95362/scratch/log.%j.err    # STDERR log
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=ma95362@uga.edu
 
@@ -17,30 +17,16 @@ module purge
 module load Bactopia/3.2.0-conda
 module load Java/17.0.6
 
-# -----------------------------
-# Define directories
-# -----------------------------
-WORKDIR="/scratch/ma95362/eth_national_analysis/all_fastq_reads"
-BACTOPIA_DIR="${WORKDIR}/ETH_paired_end_samples"
-FOFN="${WORKDIR}/all_samples.fofn"
-OUTDIR="${WORKDIR}/pangenome_results"
 
-mkdir -p "${OUTDIR}" "${WORKDIR}/logs"
-cd "${OUTDIR}"
+#Set output directory variable
+OUTDIR="/scratch/ma95362/eth_national_analysis/all_fastq_reads/"
 
-# -----------------------------
-# Run Bactopia Pangenome Workflow
-# -----------------------------
-echo "[$(date)] Starting Bactopia Pangenome Workflow..."
+# Create output folder if not exist
+mkdir -p $OUTDIR
+cd $OUTDIR
+# Run pangenome workflow using all samples
 bactopia \
     --wf pangenome \
-    --bactopia "${BACTOPIA_DIR}" \
-    --include "${FOFN}" \
-    --outdir "${OUTDIR}" \
-    --cpus ${SLURM_CPUS_PER_TASK} \
-    --force \
-    --skip_check_input \
-    --species "Mycobacterium tuberculosis complex"
-
-echo "[$(date)] ✅ Bactopia Pangenome completed successfully."
+    --bactopia "$OUTDIR/ETH_paired_end_samples" \
+    --include /scratch/ma95362/eth_national_analysis/all_fastq_reads/local_samples.txt
 
