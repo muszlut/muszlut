cf8fa9111f4707149d01b591973651f7c6e01455
muszlut <mussegirma@yahoo.com>
2025-10-10 12:10:47 +0300

    wf

--- Dissertation/DBac_Pan_TBpro_excluding.sh
+++ Dissertation/DBac_Pan_TBpro_excluding.sh
@@ -25,6 +25,7 @@ mkdir -p $OUTDIR
 cd $OUTDIR
 
 # Run pangenome workflow using FOFN
-bactopia tools pangenome \
+bactopia \
+    --wf pangenome \
     --bactopia $OUTDIR/ETH_paired_end_samples \
     --exclude /scratch/ma95362/eth_national_analysis/all_fastq_reads/ETH_paired_end_samples/bactopia-exclude_final.tsv
