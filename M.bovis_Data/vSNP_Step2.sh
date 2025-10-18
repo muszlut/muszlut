f12e0e91300acaefe338baa08d764c30b35c4b7e
muszlut <mussegirma@yahoo.com>
2025-10-18 23:33:44 +0300

    second

--- M.bovis_Data/vSNP_Step2.sh
+++ M.bovis_Data/vSNP_Step2.sh
@@ -17,17 +17,18 @@ conda activate vsnp3
 # Directories
 STEP1_OUT="/scratch/ma95362/ETH_M.bovis/m.bovis_Bactopia_Analysis/with_fixed_reads/M.bovis_paired_end_samples/all_fastqs/vSNP_output/step1_output"
 OUT_DIR="$STEP1_OUT/step2_output"
-
+REF_GBK="/home/ma95362/vsnp3_test_dataset/vsnp_dependencies/Mycobacterium_AF2122/NC_002945v4.gbk"
 mkdir -p "$OUT_DIR"
-cd "$OUT_DIR"
+cd "$STEP1_OUT"
 # Reference genome used in Step1 (adjust if needed)
 REF="/home/ma95362/vsnp3_test_dataset/vsnp_dependencies/Mycobacterium_AF2122/NC_002945v4.fasta"
 
 # Run vSNP Step2
 vsnp3_step2.py \
-    --step1_output "$STEP1_OUT" \
-    --output_dir "$OUT_DIR" \
-    --reference "$REF" \
-    --threads 32
+    -wd "$STEP1_OUT" \
+    -o "$OUT_DIR" \
+    -t fasta \
+    -b "$REF_GBK" \
+    -v -dp -html_tre
 
 echo "✅ All vSNP Step2 jobs finished successfully."
