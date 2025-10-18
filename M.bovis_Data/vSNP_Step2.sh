694cc548e0f542267069dd3f3101dba93bd72035
muszlut <mussegirma@yahoo.com>
2025-10-19 00:33:18 +0300

    third

--- M.bovis_Data/vSNP_Step2.sh
+++ M.bovis_Data/vSNP_Step2.sh
@@ -14,22 +14,25 @@
 source ~/.bashrc
 conda activate vsnp3
 
-# Directories
+# Define directories
 STEP1_OUT="/scratch/ma95362/ETH_M.bovis/m.bovis_Bactopia_Analysis/with_fixed_reads/M.bovis_paired_end_samples/all_fastqs/vSNP_output/step1_output"
 OUT_DIR="$STEP1_OUT/step2_output"
-# Load excluded sample names into memory
-EXCLUDE_FILE="/scratch/ma95362/ETH_M.bovis/m.bovis_Bactopia_Analysis/with_fixed_reads/M.bovis_paired_end_samples/all_fastqs/exclude_list.txt"
-Step1="/scratch/ma95362/ETH_M.bovis/m.bovis_Bactopia_Analysis/with_fixed_reads/M.bovis_paired_end_samples/all_fastqs/vSNP_output"/step1_output
 mkdir -p "$OUT_DIR"
-cd "$STEP1_OUT"
-# Reference genome used in Step1 (adjust if needed)
+
+# Reference genome used in Step1
 REF="/home/ma95362/vsnp3_test_dataset/vsnp_dependencies/Mycobacterium_AF2122/NC_002945v4.fasta"
 REF_GBK="/home/ma95362/vsnp3_test_dataset/vsnp_dependencies/Mycobacterium_AF2122/NC_002945v4.gbk"
 
-# Run vSNP Step2
-#Find all vcf files in the step1 output directory and move them in step2 directory
-find $Step1 -type f -name "*_zc.vcf" -exec cp {} $Step2 \;
-#move to step2 folder
-cd $OUT_DIR
-# Run vSNP step 2 to combine SNPs and build tree
-vsnp3_step2.py -a -t $REF -remove_by_name $EXCLUDE_FILE
+# Copy all *_zc.vcf files from Step1 output to Step2 directory
+find "$STEP1_OUT" -type f -name "*_zc.vcf" -exec cp {} "$OUT_DIR" \;
+
+# Move to Step2 folder
+cd "$OUT_DIR"
+
+# Run vSNP Step 2
+vsnp3_step2.py -wd "$STEP1_OUT" -o "$OUT_DIR" -t "$REF" -b "$REF_GBK" -a -html_tree
+
+# Deactivate environment
+conda deactivate
+
+echo "✅ All vSNP Step2 jobs finished successfully."
