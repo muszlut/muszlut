9ca3ce53f85bbe0e5d81c121b980149aa4b4773d
muszlut <mussegirma@yahoo.com>
2025-10-23 02:43:34 +0300

    tidy

--- Dissertation/run_Rscript.sh
+++ Dissertation/run_Rscript.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
-#SBATCH --job-name=Rscript_T3_ETH        # Job name
-#SBATCH --partition=batch             # Partition (queue) name
-#SBATCH --ntasks=1                    # Run on a single CPU
-#SBATCH --cpus-per-task=4              # Number of cores per task
-#SBATCH --mem=40gb                     # Job memory request
-#SBATCH --time=03:00:00               # Time limit hrs:min:sec
-#SBATCH --output=/scratch/ma95362/scratch/log.%j.out           # Standard output log
-#SBATCH --error=/scratch/ma95362/scratch/log.%j.err             # Standard error log
+#SBATCH --job-name=Rscript_T3_ETH
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=40gb
+#SBATCH --time=03:00:00
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ma95362@uga.edu
 
-#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=ma95362@uga.edu  # Where to send mail	
-
-#Set output directory variable
 OUTDIR="/scratch/ma95362/eth_national_analysis/all_fastq_reads/pangenome_tools_results/bactopia/bactopia-runs/pangenome_of_L4.2.2.2.2/panaroo/filtered_output/pyseer_T3ETH_out"
-#Location of R script
 TIDY="/home/ma95362/muszlut/Dissertation/T3_ETH.R"
 
+module load Miniforge3
+source ~/.bashrc
+conda activate r-tidyverse
 
-#Move to output directory
 cd $OUTDIR
-
-#Activate Renvironmemt previously created
-source activate r-tidyverse
-
-#Run R script:here
-R --no-save < $TIDY
+Rscript $TIDY
