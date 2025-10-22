67d74d7c56b70ddb07fe784aec62e9f3f905a364
muszlut <mussegirma@yahoo.com>
2025-10-23 02:33:26 +0300

    rstudi

---
+++ Dissertation/T3_ETH.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+
+
+####set seed to make this process more reproducible####
+
+
+set.seed(1989)
+
+library(data.table)
+library(qqman)
+
+# Read GWAS result
+tbl <- fread("T3-ETH_gwas.txt", data.table = FALSE)
+
+# Remove bad-chisq entries
+tbl <- tbl[!grepl("bad-chisq", tbl$notes), ]
+
+# Set threshold
+sig_threshold <- 1.21e-4
+
+# Filter significant hits
+sig_hits <- subset(tbl, `lrt-pvalue` < sig_threshold)
+
+# Save results
+write.csv(sig_hits, "T3-ETH_significant_hits.csv", row.names = FALSE, quote = FALSE)
+
+# Optional: Manhattan and QQ plots
+manhattan(tbl, p="lrt-pvalue", main="T3-ETH vs others (Pyseer GWAS)")
+qq(tbl$`lrt-pvalue`)
+qq(sig_hits$`lrt-pvalue`)   
