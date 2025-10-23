759acb24534ddbf028c3101903c4192b5f69da36
muszlut <mussegirma@yahoo.com>
2025-10-23 05:30:03 +0300

    plot

--- Dissertation/dr_gwas.R
+++ Dissertation/dr_gwas.R
@@ -24,7 +24,7 @@ tbl$SNP <- tbl$variant
 
 # Highlight top hits (e.g., mmaA4 if present)
 #highlight_variants <- c("mmaA4")
-highlight_variants <- intersect(highlight_variants, tbl$SNP)
+#highlight_variants <- intersect(highlight_variants, tbl$SNP)
 
 # Save Manhattan plot
 png("DR_manhattan_plot.png", width = 1200, height = 800)
@@ -35,7 +35,7 @@ manhattan(tbl,
           cex = 1.2, cex.axis = 1.2, cex.lab = 1.4,
           suggestiveline = FALSE,
           genomewideline = -log10(sig_threshold),
-          highlight = highlight_variants)
+          #highlight = highlight_variants)
 dev.off()
 
 # Save QQ plot (all variants)
