a6fe5b67171a9607d1efe7b0e1bca0df50b2a2e8
muszlut <mussegirma@yahoo.com>
2025-11-10 18:19:41 +0300

    all

--- Dissertation/EPTB_Hilina/Bactopia_prepare.sh
+++ Dissertation/EPTB_Hilina/Bactopia_prepare.sh
@@ -12,6 +12,12 @@
 
 # Load the Bactopia module
 module load Bactopia/3.2.0-conda
+module load EDirect/20.5.20231006-GCCcore-12.3.0
+
+bactopia search \
+    --query PRJNA1174701 \
+    --output $OUTDIR/Bactopia_Search \
+    --force
 
 # Define directories
 OUTDIR="/scratch/ma95362/EPTB_Hilina/Newe"
