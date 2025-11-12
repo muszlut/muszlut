59335307c43df984bcd3135d535d1fa395b21c3d
muszlut <mussegirma@yahoo.com>
2025-11-12 11:04:58 +0300

    alk

---
+++ Dissertation/EPTB_Hilina/FOFN_for_TBprofiler.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=Bactopia_Mtb
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=120gb
+#SBATCH --time=03-00:00:00
+#SBATCH --output=/scratch/ma95362/EPTB_Hilina/Newe/logs/log.%j.out
+#SBATCH --error=/scratch/ma95362/EPTB_Hilina/Newe/logs/log.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ma95362@uga.edu
+
+# Run as an executable file
+chmod +x generate_fofn.sh
+
+# Set the directory with your reads
+READ_DIR="/scratch/ma95362/EPTB_Hilina/reads"
+
+# Output file
+FOFN="/scratch/ma95362/EPTB_Hilina/reads/TBprofiler_reads.fofn"
+
+# Generate FOFN
+for r1 in "$READ_DIR"/*_R1.fastq.gz; do
+  base=$(basename "$r1" | sed 's/_R1.fastq.gz//' | sed 's/\.$//')
+  r2="${r1/_R1.fastq.gz/_R2.fastq.gz}"
+  echo -e "${base}\t${r1}\t${r2}"
+done > "$FOFN"
+
+echo "FOFN written to $FOFN"
---
+++ Dissertation/EPTB_Hilina/TBProfiler_conda.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#SBATCH --job-name=TBprofiler_conda
+#SBATCH --partition=highmem_p           # high memory partition for clustering
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32             # parallelize clustering
+#SBATCH --mem=240G                     # enough memory for 1398 genomes
+#SBATCH --time=7-00:00:00
+#SBATCH --output=/scratch/ma95362/EPTB_Hilina/Newe/logs/log.%j.out   # STDOUT log
+#SBATCH --error=/scratch/ma95362/EPTB_Hilina/Newe/logs/log.%j.err    # STDERR log
+#SBATCH --mail-type=END,FAIL                  # Email notifications
+#SBATCH --mail-user=ma95362@uga.edu          # Email recipient
+
+set -euo pipefail  # safer bash settings
+
+# Load Micromamba module
+module load Micromamba/2.3.0
+
+# Initialize Micromamba for bash
+eval "$(micromamba shell hook --shell bash)"
+
+# Activate TB-Profiler environment
+micromamba activate tbprofiler
+
+# Set working directories
+FASTQ_DIR="/scratch/ma95362/EPTB_Hilina/reads"
+OUTDIR="/scratch/ma95362/EPTB_Hilina/Newe/TBprofiler_results_conda"
+FOFN="/scratch/ma95362/EPTB_Hilina/Newe/TBprofiler_FOFN_samples.fofn"
+
+# Create output directory if it doesn't exist
+mkdir -p "$OUTDIR"
+cd "$OUTDIR"
+# Loop through each line in the FOFN (sample, read1, read2)
+while read -r sample read1 read2; do
+    echo "Processing $sample..."
+    SAMPLE_OUT="$OUTDIR/$sample"
+    mkdir -p "$SAMPLE_OUT"
+
+    tb-profiler profile \
+        -1 "$read1" \
+        -2 "$read2" \
+        -p "$sample" \
+        --db /home/ma95362/.conda/envs/mtbvartools/share/tbprofiler/tbdb \
+        --spoligotype \
+        --dir "$SAMPLE_OUT" \
+        --threads 8 \
+        --txt
+done < "$FOFN"
+
+# Deactivate the Micromamba environment
+micromamba deactivate
