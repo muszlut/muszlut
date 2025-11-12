1b82c92420a6f818c9d2b9a11e4afec5394e8ec2
muszlut <mussegirma@yahoo.com>
2025-11-12 18:21:23 +0300

    dd

---
+++ Dissertation/EPTB_Hilina/collate_tubprofiler.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name=TBprofiler_Mamba           # Job name
+#SBATCH --partition=batch                     # Queue/partition
+#SBATCH --ntasks=1                            # Number of tasks (1 for single-process)
+#SBATCH --cpus-per-task=8                     # Number of CPU cores
+#SBATCH --mem=40gb                            # Memory allocation
+#SBATCH --time=07-00:00:00                    # Max runtime (7 days)
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out   # STDOUT log
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err    # STDERR log
+#SBATCH --mail-type=END,FAIL                  # Email notifications
+#SBATCH --mail-user=ma95362@uga.edu           # Email recipient
+
+set -euo pipefail  # safer bash settings
+
+# Load Micromamba module
+module load Micromamba/2.3.0
+
+# Initialize Micromamba for bash
+eval "$(micromamba shell hook --shell bash)"
+
+# Activate TB-Profiler environment
+micromamba activate tbprofiler
+
+# Set working directories
+OUTDIR="/scratch/ma95362/EPTB_Hilina/Bactopia_Run/split_reads/TBprofiler_results_conda"
+FOFN="/scratch/ma95362/EPTB_Hilina/Bactopia_Run/split_reads/TBprofiler_results_conda/tbprofiler_results.fofn"
+
+# Create output directory if it doesn't exist
+mkdir -p "$OUTDIR"
+
+# Collate TB-Profiler results
+tb-profiler collate --samples samples_names.txt --dir /scratch/ma95362/EPTB_Hilina/Bactopia_Run/split_reads/TBprofiler_results_conda/*/results --itol
+
+
+# Deactivate the Micromamba environment
+micromamba deactivate
