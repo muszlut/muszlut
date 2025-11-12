3443c5f9adf2284ba7108529d623595919c4b6da
muszlut <mussegirma@yahoo.com>
2025-11-12 12:07:02 +0300

    aas

--- Dissertation/EPTB_Hilina/Bactopia_prepare.sh
+++ Dissertation/EPTB_Hilina/Bactopia_prepare.sh
@@ -11,7 +11,7 @@
 #SBATCH --mail-user=ma95362@uga.edu
 
 #OUTDIR="/scratch/ma95362/EPTB_Hilina/Newe" This was previous OUTDIR definition until we do the summary step (Step4)
-OUTDIR="/scratch/ma95362/EPTB_Hilina/"
+OUTDIR="/scratch/ma95362/EPTB_Hilina"
 # Create output directory
 mkdir -p $OUTDIR
 cd $OUTDIR
@@ -19,24 +19,24 @@ cd $OUTDIR
 # ✅ Step 0: (Optional) Download accessions list
 # -------------------------------
 #To download the accessions list first, uncomment the lines below
-module load Bactopia/3.2.0-conda
-module load EDirect/20.5.20231006-GCCcore-12.3.0
-bactopia search \
-    --query PRJNA1174701 \
-    --force \
-    --genome-size 4400000 \
-    --min-coverage 20
+#module load Bactopia/3.2.0-conda
+#module load EDirect/20.5.20231006-GCCcore-12.3.0
+#bactopia search \
+#    --query PRJNA1174701 \
+#    --force \
+#    --genome-size 4400000 \
+#    --min-coverage 20
 # -------------------------------
 # ✅ Step 1: Activate Conda first
 # -------------------------------
 # Load miniconda module if needed on Sapelo2
-#module load Miniconda3/22.11.1-1
+module load Miniconda3/22.11.1-1
 
 # Initialize Conda for non-interactive shell
-#source ~/.bashrc
+source ~/.bashrc
 
 # Activate your Bactopia Conda environment
-#conda activate bactopia
+conda activate bactopia
 
 # (Alternative — if mamba was installed)
 # mamba activate bactopia
@@ -53,11 +53,11 @@ bactopia search \
 # ✅ Step 3: Run Bactopia
 # -------------------------------
 # Option 1 — run accessions list
-#bactopia \
-#    --accessions bactopia-accessions.txt \
-#    --outdir $OUTDIR/Bactopia_Run \
-#    --species "Mycobacterium tuberculosis" \
-#    --max_cpus 16
+bactopia \
+    --accessions bactopia-accessions.txt \
+    --outdir $OUTDIR/Bactopia_Run \
+    --species "Mycobacterium tuberculosis" \
+    --max_cpus 16
 # -------------------------------
 # ✅ Step 4: Generate summary
 # -------------------------------
