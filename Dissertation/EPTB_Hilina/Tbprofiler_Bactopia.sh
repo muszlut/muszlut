1197512140d2624ae38aaac4e7ea7eacf813f60a
muszlut <mussegirma@yahoo.com>
2025-11-14 15:38:56 +0300

    dd

--- Dissertation/EPTB_Hilina/Tbprofiler_Bactopia.sh
+++ Dissertation/EPTB_Hilina/Tbprofiler_Bactopia.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=gbk_ncbi       # Job name
-#SBATCH --partition=batch             # Partition (queue) name
-#SBATCH --ntasks=1                    # Run on a single CPU
-#SBATCH --cpus-per-task=8               # Number of cores per task
-#SBATCH --mem=40gb                     # Job memory request
-#SBATCH --time=06:00:00               # Time limit hrs:min:sec
-#SBATCH --output=/scratch/ma95362/scratch/log.%j.out           # Standard output log
-#SBATCH --error=/scratch/ma95362/scratch/log.%j.err             # Standard error log
+#SBATCH --job-name=bactopia_singularity       
+#SBATCH --partition=batch            
+#SBATCH --ntasks=1                   
+#SBATCH --cpus-per-task=8              
+#SBATCH --mem=40gb                    
+#SBATCH --time=06:00:00              
+#SBATCH --output=/scratch/ma95362/EPTB_Hilina/Bactopia_Run/split_reads/log.%j.out          
+#SBATCH --error=/scratch/ma95362/EPTB_Hilina/Bactopia_Run/split_reads/log.%j.err             
 
-#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=ma95362@uga.edu  # Where to send mail	
-#Set output directory variableklkjl
+#SBATCH --mail-type=END,FAIL          
+#SBATCH --mail-user=ma95362@uga.edu  
+#Set output directory variable
 OUTDIR="/scratch/ma95362/EPTB_Hilina/Bactopia_Run/split_reads/"
-
-
 #Tell the program to make  the outdir folder
 if [ ! -d $OUTDIR ] 
     then 
@@ -22,9 +20,7 @@ fi
 module load Bactopia/3.2.0-conda
 module load EDirect/20.5.20231006-GCCcore-12.3.0
 cd $OUTDIR
-
 bactopia \
     -profile singularity \
     --wf tbprofiler \
     --bactopia $OUTDIR/split_reads
-    
