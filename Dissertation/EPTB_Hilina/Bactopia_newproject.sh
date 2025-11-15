d9b2c393ce2c3f518450fee252c5a2481256ec91
muszlut <mussegirma@yahoo.com>
2025-11-15 21:30:34 +0300

    first

--- Dissertation/EPTB_Hilina/Bactopia_newproject.sh
+++ Dissertation/EPTB_Hilina/Bactopia_newproject.sh
@@ -5,8 +5,8 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=120gb
 #SBATCH --time=03-00:00:00
-#SBATCH --output=/scratch/ma95362/EPTB_Hilina/logs/log.%j.out
-#SBATCH --error=/scratch/ma95362/EPTB_Hilina/logs/log.%j.err
+#SBATCH --output=/scratch/ma95362/EPTB_Hilina/new_project_logs/log.%j.out
+#SBATCH --error=/scratch/ma95362/EPTB_Hilina/new_project_logs/log.%j.err
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=ma95362@uga.edu
 
@@ -19,24 +19,25 @@ cd $OUTDIR
 # ✅ Step 0: (Optional) Download accessions list
 # -------------------------------
 #To download the accessions list first, uncomment the lines below
-#module load Bactopia/3.2.0-conda
+module load Bactopia/3.2.0-conda
 #module load EDirect/20.5.20231006-GCCcore-12.3.0
-#bactopia search \
-#    --query PRJNA1174701 \
-#    --force \
-#    --genome-size 4400000 \
-#    --min-coverage 20
+bactopia search \
+    --query PRJNA1174701 \
+    --include-runs \
+    --force \
+    --genome-size 4400000 \
+    --min-coverage 20
 # -------------------------------
 # ✅ Step 1: Activate Conda first
 # -------------------------------
 # Load miniconda module if needed on Sapelo2
-module load Miniconda3/22.11.1-1
+#module load Miniconda3/22.11.1-1
 
 # Initialize Conda for non-interactive shell
-source ~/.bashrc
+#source ~/.bashrc
 
 # Activate your Bactopia Conda environment
-conda activate bactopia
+#conda activate bactopia
 
 # (Alternative — if mamba was installed)
 # mamba activate bactopia
@@ -53,11 +54,11 @@ conda activate bactopia
 # ✅ Step 3: Run Bactopia
 # -------------------------------
 # Option 1 — run accessions list
-bactopia \
-    --accessions bactopia-accessions.txt \
-    --outdir $OUTDIR/Bactopia_Run \
-    --species "Mycobacterium tuberculosis" \
-    --max_cpus 16
+#bactopia \
+#    --accessions bactopia-accessions.txt \
+#    --outdir $OUTDIR/Bactopia_Run \
+#    --species "Mycobacterium tuberculosis" \
+#    --max_cpus 16
 # -------------------------------
 # ✅ Step 4: Generate summary
 # -------------------------------
