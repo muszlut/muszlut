40e1973a00905baf0b9c215597262e3b629c3136
muszlut <mussegirma@yahoo.com>
2025-11-16 16:24:46 +0300

    est

--- Dissertation/EPTB_Hilina/Bact_newproject_prep.sh
+++ Dissertation/EPTB_Hilina/Bact_newproject_prep.sh
@@ -5,20 +5,26 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=120gb
 #SBATCH --time=03-00:00:00
-#SBATCH --output=/scratch/ma95362/EPTB_Hilina/new_project_logs/curl_download_all/log.%j.out
-#SBATCH --error=/scratch/ma95362/EPTB_Hilina/new_project_logs/curl_download_all/log.%j.err
+#SBATCH --output=/scratch/ma95362/EPTB_Hilina/new_project_logs/curl_download_all/Bactopia_prepare/log.%j.out
+#SBATCH --error=/scratch/ma95362/EPTB_Hilina/new_project_logs/curl_download_all/Bactopia_prepare/log.%j.err
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=ma95362@uga.edu
 
 # -------------------------------
 # Step 0: Set Output Directory
 # -------------------------------
+# Variables
+READS_DIR="/scratch/ma95362/EPTB_Hilina/new_project/curl_download_all"
 OUTDIR="/scratch/ma95362/EPTB_Hilina/new_project_logs/curl_download_all/Bactopia_prepare"
-mkdir -p $OUTDIR
-cd $OUTDIR
+# Load Bactopia module
 module load Bactopia/3.2.0-conda
+# Create output directory if it doesn't exist
+mkdir -p "$OUTDIR"
+# Move to working directory
+cd $OUTDIR
+# Prepare samples for Bactopia
 bactopia prepare \
-    --path /scratch/ma95362/EPTB_Hilina/new_project/curl_download_all \
+    --path "$READS_DIR" \
     --species "Mycobacterium tuberculosis" \
-    --genome-size 4400000
-    > samples.txt
+    --genome-size 4410000 \
+    > "$OUTDIR/Hilina_samples.txt"
