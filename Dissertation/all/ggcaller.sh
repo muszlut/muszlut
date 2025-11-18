8ab4f0ab28713526b9e4ba63767c6da358819e6a
muszlut <mussegirma@yahoo.com>
2025-11-18 11:14:22 +0300

    pl

--- Dissertation/all/ggcaller.sh
+++ Dissertation/all/ggcaller.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=ggcaller_all
-#SBATCH --partition=highmem_p
+#SBATCH --job-name=ggcaller_all_test
+#SBATCH --partition=batch
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=240G
-#SBATCH --time=7-00:00:00
-#SBATCH --output=/scratch/ma95362/ggcaller_out/log.%j.out
-#SBATCH --error=/scratch/ma95362/ggcaller_out/log.%j.err
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=40G
+#SBATCH --time=10:00:00
+#SBATCH --output=/scratch/ma95362/test_ggcaller_out/log.%j.out
+#SBATCH --error=/scratch/ma95362/test_ggcaller_out/log.%j.err
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=ma95362@uga.edu
 
@@ -16,31 +16,18 @@ source ~/.bashrc
 conda activate ggcaller-env
 
 # Paths
-READDIR="/scratch/ma95362/ggcaller_reads"
-OUTDIR="/scratch/ma95362/ggcaller_out"
-READLIST="${OUTDIR}/reads_list.txt"
+#READDIR="/scratch/ma95362/ggcaller_reads"
+OUTDIR="/scratch/ma95362/test_ggcaller_out"
+READLIST="${OUTDIR}/test_list.txt"
 
 mkdir -p "$OUTDIR"
 
 echo "Generating reads list..."
-rm -f "$READLIST"
-
-# Auto-generate list in format: R1,R2
-for R1 in ${READDIR}/*_R1*.fastq.gz; do
-    R2=${R1/_R1/_R2}
-    if [[ -f "$R2" ]]; then
-        echo "${R1},${R2}" >> "$READLIST"
-    else
-        echo "WARNING: Missing R2 for $R1"
-    fi
-done
-
 echo "Reads list generated:"
 cat "$READLIST"
-
 echo "Running ggCaller..."
 ggcaller \
-    --reads "$READLIST" \
+    --refs "$READLIST" \
     --out "$OUTDIR" \
     --threads 16 \
     --clean-mode strict \
