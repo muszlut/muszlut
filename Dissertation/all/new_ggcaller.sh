8fe235dbab13c18eb3a3e12933b908aa2338f6fb
muszlut <mussegirma@yahoo.com>
2025-11-20 23:19:33 +0300

    ok

--- Dissertation/all/new_ggcaller.sh
+++ Dissertation/all/new_ggcaller.sh
@@ -10,31 +10,14 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=ma95362@uga.edu
 
-cd /scratch/ma95362/ggcaller_reads
+# Direct path to ggcaller binary inside your conda environment
+GG=/home/ma95362/.conda/envs/ggc_env/bin/ggcaller
 
-# Load modules or environment
-module purge
-module load miniconda3          # Or appropriate module on your cluster
+OUTDIR=test_ggcaller_out
+mkdir -p $OUTDIR
 
-# Activate conda environment
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate ggc_env
-#bifrost build \
-#    -r test_reads.txt \
-#    -k 31 \
-#    -o bifrost_graph_reads \
-#    -t 8 \
-#    --colors
-
-
-
-# Path to graph and output
-ggcaller \
-    --graph bifrost_graph_reads.gfa \
-    --colours bifrost_graph_reads.bfg_colors \
-    --out ggcaller_output \
-    --threads 8 \
-    --kmer 31
-
-# Optional: print completion message
-echo "ggCaller run finished at $(date)"
+$GG \
+    --reads1 reads_1.fq.gz \
+    --reads2 reads_2.fq.gz \
+    --outdir $OUTDIR \
+    --threads 4
