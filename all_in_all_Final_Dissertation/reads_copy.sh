26fa3046a0fafc8e4d431cec25b6039500ce0aae
muszlut <mussegirma@yahoo.com>
2025-11-24 14:24:39 +0300

    ok

---
+++ all_in_all_Final_Dissertation/reads_copy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=copy_reads_parallel
+#SBATCH --partition=batch                                # Partition (queue)
+#SBATCH --ntasks=1                                       # Single task
+#SBATCH --cpus-per-task=16                               # CPUs per task
+#SBATCH --mem=40gb                                       # Memory
+#SBATCH --time=05-00:00:00                               # Time limit (HH:MM:SS)
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out    # STDOUT log
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err     # STDERR log
+#SBATCH --mail-type=END,FAIL                             # Mail events
+#SBATCH --mail-user=ma95362@uga.edu                      # Your email
+
+# Define paths
+#SRC_DIR=/scratch/ma95362/eth_national_analysis/all_fastq_reads
+SRC_DIR=/scratch/ma95362/EPTB_Hilina/new_project/curl_download_all
+DEST_DIR=/scratch/ma95362/all_in_all_reads
+
+# Create destination if not exist
+mkdir -p $DEST_DIR
+cd $SRC_DIR
+# Copy all fastq.gz files
+cp ${SRC_DIR}/*.fastq.gz ${DEST_DIR}/
