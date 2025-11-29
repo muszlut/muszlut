d92cb288bc96c57800f7ee30ee774d49bd0d507a
muszlut <mussegirma@yahoo.com>
2025-11-29 08:24:38 +0300

    ok

---
+++ all_in_all_Final_Dissertation/snippy_bactopia.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#SBATCH --job-name=All_Snippy
+#SBATCH --partition=highmem_p
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=240G
+#SBATCH --time=7-00:00:00
+#SBATCH --output=/scratch/ma95362/all_in_all_reads/bactopia_prepare/logs/pangenome/log.%j.out
+#SBATCH --error=/scratch/ma95362/all_in_all_reads/bactopia_prepare/logs/pangenome/log.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ma95362@uga.edu
+
+# -------------------------------
+# Step 0: Set Output Directory
+# -------------------------------
+# Variables
+#READS_DIR="/scratch/ma95362/all_in_all_reads"
+OUTDIR="/scratch/ma95362/all_in_all_reads/bactopia_prepare"
+REF="/scratch/ma95362/gbk/ncbi_dataset/data/GCF_000195955.2/genomic.gbk"
+# Load Bactopia module
+module load Bactopia/3.2.0-conda
+module load Java/17.0.6
+# Create output directory if it doesn't exist
+mkdir -p "$OUTDIR"
+# Move to working directory
+cd $OUTDIR
+# Prepare samples for Bactopia
+#bactopia prepare \
+#    --path "$READS_DIR" \
+#    --species "Mycobacterium tuberculosis" \
+#    --genome-size 4410000 \
+#    > "$OUTDIR/samples.txt"
+# Run Bactopia pipeline
+#bactopia \
+#    --samples "$OUTDIR/samples.txt" \
+#    --coverage 100 \
+#    --outdir "$OUTDIR" \
+#    --max_cpus 8
+# Generate summary
+#bactopia summary \
+#    --bactopia-path "$OUTDIR"
+bactopia \
+    --wf snippy \
+    --reference "$REF" \
+    --bactopia $OUTDIR \
+    --exclude /scratch/ma95362/all_in_all_reads/bactopia_prepare/bactopia-exclude.tsv
