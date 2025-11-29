6fa891f3a66404d0498be3af04c3bc9b9bbe4b34
muszlut <mussegirma@yahoo.com>
2025-11-29 08:39:03 +0300

    ok

---
+++ all_in_all_Final_Dissertation/collate_tbprofile.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name=collate_tbprofiler      
+#SBATCH --partition=batch            
+#SBATCH --ntasks=1                   
+#SBATCH --cpus-per-task=8               
+#SBATCH --mem=40gb                     
+#SBATCH --time=06:00:00         
+#SBATCH --output=/scratch/ma95362/all_in_all_reads/bactopia_prepare/my_tbprofiler_results/log/collate/log.%j.out          
+#SBATCH --error=/scratch/ma95362/all_in_all_reads/bactopia_prepare/my_tbprofiler_results/log/collate/log.%j.err             
+#SBATCH --mail-type=END,FAIL         
+#SBATCH --mail-user=ma95362@uga.edu  
+#Set output directory variable
+OUTDIR="/scratch/ma95362/all_in_all_reads/bactopia_prepare/my_tbprofiler_results"
+FOFN='/scratch/ma95362/all_in_all_reads/bactopia_prepare/my_tbprofiler_results'
+
+
+#Tell the program to make  the outdir folder
+if [ ! -d $OUTDIR ] 
+    then 
+        mkdir -p $OUTDIR
+fi
+
+#Load modules
+module load Miniconda3/23.5.2-0
+
+#activate conda envirnoment (should be version 6.3.0)
+
+source activate tb-profiler-env
+
+#move to working directory:
+cd $OUTDIR
+
+#samples just needs to be a list of sample names. No path is required.
+tb-profiler collate --samples $FOFN/samples_only.txt --dir $OUTDIR --itol
