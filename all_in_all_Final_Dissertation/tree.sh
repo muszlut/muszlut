8ce1e8a892d4b3aaac86df5a5b4b4b287e4f11d7
muszlut <mussegirma@yahoo.com>
2025-12-02 15:55:13 +0300

    ok

---
+++ all_in_all_Final_Dissertation/tree.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=iqtree_core         # Job name
+#SBATCH --output=iqtree_core_%j.out    # Standard output file
+#SBATCH --error=iqtree_core_%j.err     # Standard error file
+#SBATCH --ntasks=1                      # Number of tasks (1 for IQ-TREE)
+#SBATCH --cpus-per-task=8               # Number of CPU cores for IQ-TREE
+#SBATCH --time=12:00:00                 # Max runtime (adjust as needed)
+#SBATCH --mem=16G                       # Memory (adjust as needed)
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ma95362@uga.edu
+
+# ------------------------------
+# 1. Activate Conda environment
+# ------------------------------
+module load Miniforge3
+source activate pyseer-env
+
+# Run IQ-TREE
+iqtree -s core_gene_alignment.aln -pre core_tree -nt 8 -fast -m GTR
