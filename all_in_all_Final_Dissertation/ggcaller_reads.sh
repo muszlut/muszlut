485e14634fbe3a8e35b4188d954476594f9fc059
muszlut <mussegirma@yahoo.com>
2025-12-04 15:10:51 +0300

    okk

--- all_in_all_Final_Dissertation/ggcaller_reads.sh
+++ all_in_all_Final_Dissertation/ggcaller_reads.sh
@@ -19,9 +19,9 @@ module load ggCaller/1.4.1
 # -------------------------------
 # Define paths
 # -------------------------------
-READS_LIST=/scratch/ma95362/all_in_all_reads/small_reads.txt   # list of all paired-end samples
-REFS_LIST=/scratch/ma95362/all_in_all_reads/refs_list.txt      # curated reference genomes
-BALROG_DB=/scratch/ma95362/ggcaller_db
+#READS_LIST=/scratch/ma95362/all_in_all_reads/small_reads.txt   # list of all paired-end samples
+#REFS_LIST=/scratch/ma95362/all_in_all_reads/refs_list.txt      # curated reference genomes
+#BALROG_DB=/scratch/ma95362/ggcaller_db
 OUTDIR=/scratch/ma95362/all_in_all_reads/ggcaller_results
 
 # -------------------------------
@@ -39,12 +39,12 @@ echo "Job started at $(date)"
 # Run ggCaller
 # -------------------------------
 ggcaller \
-    --reads "${READS_LIST}" \
-    --refs "${REFS_LIST}" \
+    --reads-list /scratch/ma95362/all_in_all_reads/small_reads.txt \
+    --refs /scratch/ma95362/all_in_all_reads/refs_list.txt \
     --annotation ultrasensitive \
     --alignment pan \
     --aligner def \
-    --balrog-db "${BALROG_DB}" \
+    --balrog-db /scratch/ma95362/ggcaller_db \
     --threads 32 \
     --save \
     --out "${OUTDIR}"
