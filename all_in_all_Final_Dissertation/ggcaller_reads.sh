4f52eb47ed1582da76eb13603d2b99744edb7246
muszlut <mussegirma@yahoo.com>
2025-12-04 15:18:59 +0300

    ok

--- all_in_all_Final_Dissertation/ggcaller_reads.sh
+++ all_in_all_Final_Dissertation/ggcaller_reads.sh
@@ -38,16 +38,15 @@ echo "Job started at $(date)"
 # -------------------------------
 # Run ggCaller
 # -------------------------------
-ggcaller \
-    --reads /scratch/ma95362/all_in_all_reads/small_reads.txt \
-    --refs /scratch/ma95362/all_in_all_reads/refs_list.txt \
-    --annotation ultrasensitive \
-    --alignment pan \
-    --aligner def \
-    --balrog-db /scratch/ma95362/ggcaller_db/ggCallerdb \
-    --threads 32 \
-    --save \
-    --out /scratch/ma95362/all_in_all_reads/ggcaller_results
+ggcaller --reads /scratch/ma95362/all_in_all_reads/small_reads_fixed.txt \
+         --refs /scratch/ma95362/all_in_all_reads/refs_list.txt \
+         --balrog-db /scratch/ma95362/ggcaller_db/ggCallerdb \
+         --annotation ultrasensitive \
+         --alignment pan \
+         --aligner def \
+         --threads 32 \
+         --save \
+         --out /scratch/ma95362/all_in_all_reads/ggcaller_results
 
 # -------------------------------
 # Check if ggCaller ran successfully
