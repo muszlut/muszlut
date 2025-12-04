e97059b85327c261ba4891fa6e29e76c1eca82c3
muszlut <mussegirma@yahoo.com>
2025-12-04 14:39:18 +0300

    small smaples

--- all_in_all_Final_Dissertation/ggcaller_reads.sh
+++ all_in_all_Final_Dissertation/ggcaller_reads.sh
@@ -19,7 +19,7 @@ module load ggCaller/1.4.1
 # -------------------------------
 # Define paths
 # -------------------------------
-READS_LIST=/scratch/ma95362/all_in_all_reads/input_reads.txt   # list of all paired-end samples
+READS_LIST=/scratch/ma95362/all_in_all_reads/small_reads.txt   # list of all paired-end samples
 REFS_LIST=/scratch/ma95362/all_in_all_reads/refs_list.txt      # curated reference genomes
 BALROG_DB=/scratch/ma95362/ggcaller_db
 OUTDIR=/scratch/ma95362/all_in_all_reads/ggcaller_results
