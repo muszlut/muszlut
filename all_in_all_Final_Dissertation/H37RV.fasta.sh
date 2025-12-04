994769a0c7006390613bd0c7a493b47b34fa9250
muszlut <mussegirma@yahoo.com>
2025-12-04 12:42:13 +0300

    okkkk

--- all_in_all_Final_Dissertation/H37RV.fasta.sh
+++ all_in_all_Final_Dissertation/H37RV.fasta.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
-#SBATCH --job-name=Ensemble)_download_ethiopia        # Job name
-#SBATCH --partition=batch             # Partition (queue) name
-#SBATCH --ntasks=1                    # Run on a single CPU
-#SBATCH --mem=1gb                     # Job memory request
-#SBATCH --time=02:00:00               # Time limit hrs:min:sec
-#SBATCH --output=/scratch/ma95362/scratch/log.%j.out           # Standard output log
-#SBATCH --error=/scratch/ma95362/scratch/log.%j.err             # Standard error log
+#SBATCH --job-name=download_H37Rv
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --mem=1gb
+#SBATCH --time=02:00:00
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ma95362@uga.edu  
 
-#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=ma95362@uga.edu  # Where to send mail	
+OUTDIR="/scratch/ma95362/ggcaller_module/reference"
 
-#Set output directory variable
-OUTDIR="/scratch/ma95362/ggcaller_module/referenece"
-#Tell the program to make  the outdir folder
-if [ ! -d $OUTDIR ] 
-    then 
-        mkdir -p $OUTDIR
+if [ ! -d $OUTDIR ]; then 
+    mkdir -p $OUTDIR
 fi
-cd $OUTDIR
-#Set variable URL for genome website
-URL=" https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/195/955/GCF_000195955.2_ASM19595v2/GCF_000195955.2_ASM19595v2_genomic.fna.gz "
 
-#gunzip -c GCF_000195955.2_ASM19595v2_genomic.fna.gz
+cd $OUTDIR
+
+# Correct URL (no spaces!)
+URL="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/195/955/GCF_000195955.2_ASM19595v2/GCF_000195955.2_ASM19595v2_genomic.fna.gz"
+
+# Download H37Rv
+wget $URL
+
+# Unzip
+gunzip GCF_000195955.2_ASM19595v2_genomic.fna.gz
