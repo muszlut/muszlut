fc25bd58077dfbf5d2f1d13064cda7d1ca56a70b
muszlut <mussegirma@yahoo.com>
2025-12-05 16:55:08 +0300

    1000

--- all_in_all_Final_Dissertation/ggcaller_pan_all.sh
+++ all_in_all_Final_Dissertation/ggcaller_pan_all.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=highmem_p
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=2000G
+#SBATCH --mem=1000G
 #SBATCH --time=7-00:00:00
 #SBATCH --output=/scratch/ma95362/ggcaller_module/logs/log.%j.out
 #SBATCH --error=/scratch/ma95362/ggcaller_module/logs/log.%j.err
