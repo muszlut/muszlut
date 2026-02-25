d24b612e3a1176c992b2f7234e6896f96dbf5d05
muszlut <mussegirma@yahoo.com>
2026-02-25 23:57:37 +0300

    a

---
+++ jigjiga_isolates/eth_national.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#SBATCH --job-name=bp_700_mtbc
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=200gb
+#SBATCH --time=07-00:00:00
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ma95362@uga.edu
+
+set -euo pipefail
+
+# -------------------------------
+# Load Modules
+# -------------------------------
+module load Bactopia/3.2.0-conda
+module load Java/17.0.6
+
+# -------------------------------
+# Directories
+# -------------------------------
+OUTDIR="/scratch/ma95362/publication/eth_3rd_national_dataset"
+SEARCHDIR="$OUTDIR/search"
+RUNNAME="ena_prjna1104194_700"
+
+mkdir -p "$SEARCHDIR"
+cd "$OUTDIR"
+
+# -------------------------------
+# Step 1: Search SRA
+# -------------------------------
+bactopia search \
+    --query PRJNA1104194 \
+    --datasets sra \
+    --outdir "$SEARCHDIR"
+
+# -------------------------------
+# Step 2: Run Full Pipeline
+# -------------------------------
+bactopia \
+    --accessions "$SEARCHDIR/bactopia-accessions.txt" \
+    --coverage 30 \
+    --outdir "$OUTDIR/$RUNNAME" \
+    --max_cpus $SLURM_CPUS_PER_TASK
