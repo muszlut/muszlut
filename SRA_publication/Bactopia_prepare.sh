58ec1d2f9b29e85517d8f30d2741056d464c02e6
muszlut <mussegirma@yahoo.com>
2026-05-16 22:36:46 +0300

    k

--- SRA_publication/Bactopia_prepare.sh
+++ SRA_publication/Bactopia_prepare.sh
@@ -18,18 +18,8 @@
 READS_DIR="/scratch/ma95362/clean_sequences_reads"
 OUTDIR="/scratch/ma95362/SRA_publication"
 #REF="/scratch/ma95362/gbk/ncbi_dataset/data/GCF_000195955.2/genomic.gbk"
-
-export TMPDIR=/scratch/ma95362/tmp
-mkdir -p $TMPDIR
-
-# Clean env conflicts
-unset CONDA_PREFIX
-unset CONDA_DEFAULT_ENV
-
 # Load Bactopia module
-module purge
 module load Bactopia/4.0.0-conda
-module load Java/17.0.6
 # Create output directory if it doesn't exist
 mkdir -p "$OUTDIR"
 # Move to working directory
