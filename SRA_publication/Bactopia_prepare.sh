700db5da5465699c34f128bb8d7ccb7e8f8e6cfc
muszlut <mussegirma@yahoo.com>
2026-05-21 17:18:00 +0300

    ok

--- SRA_publication/Bactopia_prepare.sh
+++ SRA_publication/Bactopia_prepare.sh
@@ -1,39 +1,61 @@
 #!/bin/bash
-#SBATCH --job-name=SRA                                         # Job name
-#SBATCH --partition=batch                                      # Partition (queue) name
-#SBATCH --ntasks=1                                             # Run on a single CPU
-#SBATCH --cpus-per-task=16                                     # Number of cores per task
-#SBATCH --mem=120gb                                            # Job memory request
-#SBATCH --time=07-00:00:00                                     # Time limit hrs:min:sec
+set -e
+
+#SBATCH --job-name=SRA
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=120gb
+#SBATCH --time=07-00:00:00
 #SBATCH --gres=lscratch:100
-#SBATCH --output=/scratch/ma95362/scratch/log.%j.out           # Standard output log
-#SBATCH --error=/scratch/ma95362/scratch/log.%j.err            # Standard error log
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ma95362@uga.edu
 
-#SBATCH --mail-type=END,FAIL                                   # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=ma95362@uga.edu                            # Where to send mail	
-
-# -------------------------------
-# Step 0: Set Output Directory
 # -------------------------------
 # Variables
+# -------------------------------
 READS_DIR="/scratch/ma95362/clean_sequences_reads"
 OUTDIR="/scratch/ma95362/SRA_publication"
-#REF="/scratch/ma95362/gbk/ncbi_dataset/data/GCF_000195955.2/genomic.gbk"
-# Load Bactopia module
+
+# -------------------------------
+# Load Bactopia
+# -------------------------------
 module load Bactopia/4.0.0-conda
+
+# -------------------------------
+# Use lscratch for Bactopia cache
+# -------------------------------
 export BACTOPIA_CACHE=/lscratch/$USER/bactopia-cache
-mkdir -p $BACTOPIA_CACHE/{conda,datasets,singularity}
-# Create output directory if it doesn't exist
+
+mkdir -p /lscratch/$USER
+mkdir -p $BACTOPIA_CACHE/conda
+mkdir -p $BACTOPIA_CACHE/datasets
+mkdir -p $BACTOPIA_CACHE/singularity
+
+# -------------------------------
+# Create output directory
+# -------------------------------
 mkdir -p "$OUTDIR"
-# Move to working directory
-cd $OUTDIR
-# Prepare samples for Bactopia
+
+# -------------------------------
+# Move to output directory
+# -------------------------------
+cd "$OUTDIR"
+
+# -------------------------------
+# Prepare samples
+# -------------------------------
 bactopia prepare \
     --path "$READS_DIR" \
     --species "Mycobacterium tuberculosis" \
     --genome-size 4410000 \
     > "$OUTDIR/samples.txt"
-# Run Bactopia pipeline
+
+# -------------------------------
+# Run Bactopia
+# -------------------------------
 bactopia \
     --samples "$OUTDIR/samples.txt" \
     --coverage 100 \
