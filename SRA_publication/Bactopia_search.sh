38b2f8fc5749694acd24f95153fa2afddf70089c
muszlut <mussegirma@yahoo.com>
2026-06-03 14:41:19 +0300

    ok

--- SRA_publication/Bactopia_search.sh
+++ SRA_publication/Bactopia_search.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=Bactopia_search
 #SBATCH --partition=batch
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=120gb
 #SBATCH --time=07-00:00:00
 #SBATCH --output=/scratch/ma95362/scratch/log.%j.out
@@ -39,5 +39,10 @@ cd "$OUTDIR"
 # -------------------------------
 # Prepare samples
 # -------------------------------
-bactopia search \
-    --query PRJNA1392122
+#bactopia search \
+#    --query PRJNA1392122
+bactopia \
+    --accessions bactopia-accessions.txt \
+    --coverage 100 \
+    --outdir ena-multiple-samples \
+    --max_cpus 16
