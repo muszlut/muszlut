e745cbcbcd2a1c9b859ec0eb0d4d0f67ae57c64a
muszlut <mussegirma@yahoo.com>
2026-06-18 17:07:41 +0300

    ok

---
+++ SRA_publication/full_BaktaDB.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=bakta_db
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=80G
+#SBATCH --time=07-00:00:00
+#SBATCH --output=/scratch/ma95362/bakta_logs/bakta_db_%j.out
+#SBATCH --error=/scratch/ma95362/bakta_logs/bakta_db_%j.err
+
+mkdir -p /scratch/ma95362/bakta_logs
+mkdir -p /scratch/ma95362/full_bakta_db
+cd /scratch/ma95362/full_bakta_db
+singularity exec /apps/singularity-images/ggcallaroo_v0.1.0.sif \
+bakta_db download \
+    --output /scratch/ma95362/full_bakta_db \
+    --type full
