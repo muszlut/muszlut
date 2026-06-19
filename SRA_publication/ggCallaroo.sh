1d7ffb534b6d037c3427c847b2acbd3aca08bb5c
muszlut <mussegirma@yahoo.com>
2026-06-19 18:13:34 +0300

    first

---
+++ SRA_publication/ggCallaroo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=ggCallaroo
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=120gb
+#SBATCH --time=07-00:00:00
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ma95362@uga.edu
+
+cd /scratch/ma95362/257_assembled_files/MTB_ggcaller/test-ggcallaroo_results
+
+singularity exec /apps/singularity-images/ggcallaroo_v0.1.0.sif \
+snakemake \
+-s /app/Snakefile \
+--cores 32 \
+--config \
+refs=/scratch/ma95362/257_assembled_files/MTB_ggcaller/test-ggcallaroo_results/test_refs.txt \
+balrog-db=/scratch/ma95362/ggcaller_db \
+bakta_db=/scratch/ma95362/full_bakta_db/db \
+output_dir=/scratch/ma95362/257_assembled_files/MTB_ggcaller/test-ggcallaroo_results \
+ggcaller_cli_args="--save" \
+panaroo_cli_args="--clean-mode moderate"
