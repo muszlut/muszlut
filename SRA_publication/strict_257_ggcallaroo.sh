de104f24f16f6dba953e1168e4c203aa68c60077
muszlut <mussegirma@yahoo.com>
2026-06-20 06:13:53 +0300

    ok

---
+++ SRA_publication/strict_257_ggcallaroo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=ggCallaroo
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=120gb
+#SBATCH --time=07-00:00:00
+#SBATCH --output=/scratch/ma95362/scratch/log.%j.out
+#SBATCH --error=/scratch/ma95362/scratch/log.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ma95362@uga.edu
+
+export TMPDIR=/scratch/ma95362/tmp
+mkdir -p $TMPDIR
+
+singularity exec /apps/singularity-images/ggcallaroo_v0.1.0.sif \
+snakemake \
+-s /app/Snakefile \
+--directory /scratch/ma95362/257_assembled_files/MTB_ggcaller/strict_257-ggcallaroo_results \
+--cores 32 \
+--use-conda \
+--config \
+refs=/scratch/ma95362/257_assembled_files/MTB_ggcaller/strict_257-ggcallaroo_results/input.txt \
+bakta_db=/scratch/ma95362/full_bakta_db/db \
+output_dir=/scratch/ma95362/257_assembled_files/MTB_ggcaller/strict_257-ggcallaroo_results \
+ggcaller_cli_args="--save --balrog-db /scratch/ma95362/ggcaller_db" \
+panaroo_cli_args="--clean-mode strict -a core --core_threshold 0.99"
