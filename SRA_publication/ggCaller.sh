39efa36e5962ea60440b4386c16487c34a669abd
muszlut <mussegirma@yahoo.com>
2026-06-20 06:41:29 +0300

    ok

--- SRA_publication/ggCaller.sh
+++ SRA_publication/ggCaller.sh
@@ -21,36 +21,52 @@ cd /scratch/ma95362/257_assembled_files
 # ደረጃ 1: ለ ggCaller የሚያስፈልገውን የፋይል ዝርዝር ማዘጋጀት
 # ------------------------------------------------------------------------------
 
-echo "1. የተገጣጠሙ ፋይሎች (Assembly FASTA/fna) ዝርዝር እየተዘጋጀ ነው (input.txt)..."
+#echo "1. የተገጣጠሙ ፋይሎች (Assembly FASTA/fna) ዝርዝር እየተዘጋጀ ነው (input.txt)..."
 
 # ማስተካከያ: .fasta ወደ .fna.gz ተቀይሯል. ggCaller የ .gz (Gzip) ቅርጸትን ማንበብ ይችላል።
-ls -d -1 $PWD/*.fna > input.txt 
+#ls -d -1 $PWD/*.fna > input.txt 
 
-if [ -f "input.txt" ]; then
-    echo "ዝርዝሩ በ input.txt ላይ ተቀምጧል:"
-    wc -l input.txt
-else
-    echo "!!! ስህተት: input.txt አልተፈጠረም። የፋይል ቅጥያዎ (.fna) ትክክል መሆኑን ያረጋግጡ።"
-    exit 1
-fi
+#if [ -f "input.txt" ]; then
+#    echo "ዝርዝሩ በ input.txt ላይ ተቀምጧል:"
+#    wc -l input.txt
+#else
+#    echo "!!! ስህተት: input.txt አልተፈጠረም። የፋይል ቅጥያዎ (.fna) ትክክል መሆኑን ያረጋግጡ።"
+#    exit 1
+#fi
 
 # ------------------------------------------------------------------------------
 # ደረጃ 2: ggCallerን ማስኬድ (--refs ተጠቅመን)
 # ------------------------------------------------------------------------------
 
-echo "2. ggCaller በ --refs mode (Assembly FNA) እየተጀመረ ነው..."
+#echo "2. ggCaller በ --refs mode (Assembly FNA) እየተጀመረ ነው..."
 
 # --refs የሚለው አማራጭ Assembly ፋይሎችን ማንበብ እንዲችል ያደርገዋል።
-singularity exec /apps/singularity-images/ggcaller_v1.5.0.sif \
-ggcaller \
-  --refs input.txt \
-  --gene-finding-only \
-  --save \
-  --threads 32 \
-  --balrog-db /scratch/ma95362/ggcaller_db \
-  --out MTB_ggcaller
+#singularity exec /apps/singularity-images/ggcaller_v1.5.0.sif \
+#ggcaller \
+#  --refs input.txt \
+#  --gene-finding-only \
+#  --save \
+#  --threads 32 \
+#  --balrog-db /scratch/ma95362/ggcaller_db \
+#  --out MTB_ggcaller
 
 
 # ------------------------------------------------------------------------------
 # መጨረሻ
 # ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ደረጃ 3: ggCallerን ማስኬድ (--refs ተጠቅመን)
+# ------------------------------------------------------------------------------
+
+#echo "3. ggCaller በ core genome alignment እየተጀመረ ነው..."
+
+singularity exec /apps/singularity-images/ggcaller_v1.5.0.sif \
+ggcaller \
+  --refs input.txt \
+  --save \
+  --threads 32 \
+  --balrog-db /scratch/ma95362/ggcaller_db \
+  --alignment core \
+  --aligner def \
+  --core-threshold 0.99 \
+  --out Alignment_results
