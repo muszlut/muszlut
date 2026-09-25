d377e8ef12424beb92310a280f205a68cf22b5cb
muszlut <mussegirma@yahoo.com>
2026-09-25 17:29:02 +0300

    ok

--- SRA_publication/Bactopia_prepare.sh
+++ SRA_publication/Bactopia_prepare.sh
@@ -3,51 +3,44 @@
 #SBATCH --partition=batch
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
-#SBATCH --mem=120gb
+#SBATCH --mem=120G
 #SBATCH --time=07-00:00:00
 #SBATCH --output=/scratch/ma95362/scratch/log.%j.out
 #SBATCH --error=/scratch/ma95362/scratch/log.%j.err
-
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=ma95362@uga.edu
+
+set -euo pipefail
+
+# Load Mamba
+module load Mamba/23.11.0-0
+
+# Activate Bactopia environment
+source activate bactopia4
+
 # -------------------------------
 # Variables
 # -------------------------------
 READS_DIR="/scratch/ma95362/clean_sequences_reads"
-OUTDIR="/scratch/ma95362/SRA_publication"
-REF="/scratch/ma95362/gbk/ncbi_dataset/data/GCF_000195955.2/genomic.gbk"
-# -------------------------------
-# Load Bactopia
-# -------------------------------
-module purge
-module load Bactopia/4.0.0-conda
-
-# -------------------------------
-# Use lscratch for Bactopia cache
-# -------------------------------
-#export BACTOPIA_CACHE=/lscratch/ma95362/bactopia-cache
-#mkdir -p $BACTOPIA_CACHE/{conda,datasets,singularity}
+OUTDIR="/scratch/ma95362/Bio_project_publication"
+#REF="/scratch/ma95362/gbk/ncbi_dataset/data/GCF_000195955.2/genomic.gbk"
 
 # -------------------------------
 # Create output directory
 # -------------------------------
 mkdir -p "$OUTDIR"
-
-# -------------------------------
-# Move to output directory
-# -------------------------------
 cd "$OUTDIR"
 
 # -------------------------------
 # Prepare samples
 # -------------------------------
-#bactopia prepare \
-#    --path "$READS_DIR" \
-#    --species "Mycobacterium tuberculosis" \
-#    --genome-size 4410000 \
-#    > "$OUTDIR/samples.txt"
-#
-# -------------------------------
+bactopia prepare \
+    --path "$READS_DIR" \
+    --species "Mycobacterium tuberculosis" \
+    --genome-size 4410000 \
+    > "$OUTDIR/samples.txt"
+
+## -------------------------------
 # Run Bactopia
 # -------------------------------
 #bactopia \
@@ -55,18 +48,13 @@ cd "$OUTDIR"
 #    --coverage 100 \
 #    --outdir "$OUTDIR" \
 #    --max_cpus 8 \
-#    --max_memory 60.GB \
-#    -resume
-# Generate summary
-######### Clean bad version files
-#find "$OUTDIR" -name "versions.yml" -delete
-######### Run summary
+#    --max_memory 60.GB 
+
 #bactopia summary --bactopia-path "$OUTDIR"
-bactopia \
-    --wf snippy \
-    --reference $REF \
-    --bactopia $OUTDIR \
-    -resume
+#bactopia \
+#    --wf snippy \
+#    --reference $REF \
+#    --bactopia $OUTDIR 
 #bactopia \
 #    --wf pangenome \
 #    --bactopia $OUTDIR
